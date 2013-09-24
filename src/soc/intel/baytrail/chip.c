71ee403179d4b76b3ad066463c72877e38192ad2
Aaron Durbin <adurbin@chromium.org>
2013-09-24 16:47:49 -0500

    baytrail: introduce pattrs
    The pattrs structure is intended for the supporting coreboot
    code to reference instead of going back to the source of
    the values (msrs, cpuid, etc). It essentially serves as a global
    structure for collecting attributes about the platform/processor.
    
    Additionally, the implementation provides a point during boot to
    hoook work before device enumeration/initialization by providing
    a init() function to soc_intel_baytrail_ops that is called before
    device work in the boot state machine.
    
    BUG=chrome-os-partner:22862
    BUG=chrome-os-partner:22863
    BRANCH=None
    TEST=Built and booted. Noted pattrs output.
    
    Change-Id: I073da8aca29635146fb0d4a2625b2b7564fd8414
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/170403
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- src/soc/intel/baytrail/chip.c
+++ src/soc/intel/baytrail/chip.c
@@ -20,6 +20,8 @@
 #include <console/console.h>
 #include <device/device.h>
 #include <device/pci.h>
+
+#include <baytrail/ramstage.h>
 #include "chip.h"
 
 static void pci_domain_set_resources(device_t dev)
@@ -64,9 +66,16 @@ static void enable_dev(device_t dev)
 	}
 }
 
+/* Called at BS_DEV_INIT_CHIPS time -- very early. Just after BS_PRE_DEVICE. */
+static void soc_init(void *chip_info)
+{
+	baytrail_init_pre_device();
+}
+
 struct chip_operations soc_intel_baytrail_ops = {
 	CHIP_NAME("Intel BayTrail SoC")
 	.enable_dev = enable_dev,
+	.init = soc_init,
 };
 
 static void pci_set_subsystem(device_t dev, unsigned vendor, unsigned device)
