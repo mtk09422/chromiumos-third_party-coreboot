3eca23627ce7f720819046f84114e969207a0604
Stefan Reinauer <reinauer@chromium.org>
2013-10-17 16:59:36 -0700

    beltino: Use ChromeBox thermals
    taken from Stumpy
    
    BUG=none
    BRANCH=none
    TEST=Boot ChromeOS
    
    Change-Id: I1e216b854fe342b8c2101af5be421e56d6d1c67d
    Signed-off-by: Stefan Reinauer <reinauer@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/173641
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>
    Commit-Queue: Stefan Reinauer <reinauer@chromium.org>
    Tested-by: Stefan Reinauer <reinauer@chromium.org>

--- src/mainboard/google/beltino/acpi_tables.c
+++ src/mainboard/google/beltino/acpi_tables.c
@@ -44,12 +44,30 @@ unsigned long acpi_create_slic(unsigned long current);
 
 static void acpi_update_thermal_table(global_nvs_t *gnvs)
 {
-	gnvs->tmps = TEMPERATURE_SENSOR_ID;
+	gnvs->f4of = FAN4_THRESHOLD_OFF;
+	gnvs->f4on = FAN4_THRESHOLD_ON;
+	gnvs->f4pw = FAN4_PWM;
+
+	gnvs->f3of = FAN3_THRESHOLD_OFF;
+	gnvs->f3on = FAN3_THRESHOLD_ON;
+	gnvs->f3pw = FAN3_PWM;
+
+	gnvs->f2of = FAN2_THRESHOLD_OFF;
+	gnvs->f2on = FAN2_THRESHOLD_ON;
+	gnvs->f2pw = FAN2_PWM;
+
+	gnvs->f1of = FAN1_THRESHOLD_OFF;
+	gnvs->f1on = FAN1_THRESHOLD_ON;
+	gnvs->f1pw = FAN1_PWM;
+
+	gnvs->f0of = FAN0_THRESHOLD_OFF;
+	gnvs->f0on = FAN0_THRESHOLD_ON;
+	gnvs->f0pw = FAN0_PWM;
+
 	gnvs->tcrt = CRITICAL_TEMPERATURE;
 	gnvs->tpsv = PASSIVE_TEMPERATURE;
 	gnvs->tmax = MAX_TEMPERATURE;
-	gnvs->f0pw = EC_THROTTLE_POWER_LIMIT;
-	gnvs->flvl = 1;
+	gnvs->flvl = 5;
 }
 
 static void acpi_create_gnvs(global_nvs_t *gnvs)
