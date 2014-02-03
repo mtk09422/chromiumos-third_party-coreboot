ee4620a90a131dce49f96b2da7f0a3bb70b13115
Aaron Durbin <adurbin@chromium.org>
2014-02-03 16:50:04 -0600

    rambi: configure wake pins as just wake sources
    GPIO_ACPI_SCI implies wake and generating an SCI. This
    is unnecessary. Therefore, configure the intended wake
    pins to only just wake by using GPIO_ACPI_WAKE.
    
    BUG=none
    BRANCH=baytrail
    TEST=Manual. S3 resumes from keyboard and trackpad. No
         excessive SCI's are generated.
    
    Change-Id: I0b841e64909301e8d4789183ad53a016be7120f1
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/184719
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/mainboard/google/rambi/gpio.c
+++ src/mainboard/google/rambi/gpio.c
@@ -163,9 +163,9 @@ static const struct soc_gpio_map gpscore_gpio_map[] = {
 
 /* SSUS GPIOs */
 static const struct soc_gpio_map gpssus_gpio_map[] = {
-	GPIO_ACPI_SCI,	/* S500 - PCH_WAKE# */
-	GPIO_ACPI_SCI,	/* S501 - TRACKPAD_INT# - INT */
-	GPIO_ACPI_SCI,	/* S502 - TOUCH_INT# - INT */
+	GPIO_ACPI_WAKE,	/* S500 - PCH_WAKE# */
+	GPIO_ACPI_WAKE,	/* S501 - TRACKPAD_INT# - INT */
+	GPIO_ACPI_WAKE,	/* S502 - TOUCH_INT# - INT */
 	GPIO_FUNC6,	/* S503 - LTE_WAKE_L# - INT */
 	GPIO_NC,	/* S504 - SOC_JTAG2_TDO (NC/PU) */
 	GPIO_FUNC1,	/* S505 - SUS_CLK_WLAN (NC) */
