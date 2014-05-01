86ef1a45a2e5f307467b3be48e377569f37b3068
Duncan Laurie <dlaurie@chromium.org>
2014-04-30 20:36:55 -0700

    broadwell: pch.h: split USB into new headers
    Move the XHCI and EHCI related register/bit defines into a separate
    header file at broadwell/ehci.h and broadwell/xhci.h
    
    BUG=chrome-os-partner:28234
    TEST=None
    
    Change-Id: I24df90c797ebdc5dee1fe84ed57c565c5360dd1c
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/198554
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/broadwell/broadwell/pch.h
+++ src/soc/intel/broadwell/broadwell/pch.h
@@ -89,10 +89,6 @@
 
 #if defined (__SMM__) && !defined(__ASSEMBLER__)
 void intel_pch_finalize_smm(void);
-void usb_ehci_sleep_prepare(device_t dev, u8 slp_typ);
-void usb_ehci_disable(device_t dev);
-void usb_xhci_sleep_prepare(device_t dev, u8 slp_typ);
-void usb_xhci_route_all(void);
 #endif
 
 #if !defined(__ASSEMBLER__)
@@ -161,52 +157,6 @@ void pch_enable_lpc(void);
 #define PCH_XHCI_DEV		PCI_DEV(0, 0x14, 0)
 #define PCH_ME_DEV		PCI_DEV(0, 0x16, 0)
 #define PCH_PCIE_DEV_SLOT	28
-
-/* EHCI PCI Registers */
-#define EHCI_PWR_CTL_STS	0x54
-#define  PWR_CTL_SET_MASK	0x3
-#define  PWR_CTL_SET_D0		0x0
-#define  PWR_CTL_SET_D3		0x3
-#define  PWR_CTL_ENABLE_PME	(1 << 8)
-#define  PWR_CTL_STATUS_PME	(1 << 15)
-
-/* EHCI Memory Registers */
-#define EHCI_USB_CMD		0x20
-#define  EHCI_USB_CMD_RUN	(1 << 0)
-#define  EHCI_USB_CMD_PSE	(1 << 4)
-#define  EHCI_USB_CMD_ASE	(1 << 5)
-#define EHCI_PORTSC(port)	(0x64 + (port * 4))
-#define  EHCI_PORTSC_ENABLED	(1 << 2)
-#define  EHCI_PORTSC_SUSPEND	(1 << 7)
-
-/* XHCI PCI Registers */
-#define XHCI_PWR_CTL_STS	0x74
-#define XHCI_USB2PR		0xd0
-#define XHCI_USB2PRM		0xd4
-#define  XHCI_USB2PR_HCSEL	0x7fff
-#define XHCI_USB3PR		0xd8
-#define  XHCI_USB3PR_SSEN	0x3f
-#define XHCI_USB3PRM		0xdc
-#define XHCI_USB3FUS		0xe0
-#define  XHCI_USB3FUS_SS_MASK	3
-#define  XHCI_USB3FUS_SS_SHIFT	3
-#define XHCI_USB3PDO		0xe8
-
-/* XHCI Memory Registers */
-#define XHCI_USB3_PORTSC(port)	((pch_is_lp() ? 0x510 : 0x570) + (port * 0x10))
-#define  XHCI_USB3_PORTSC_CHST	(0x7f << 17)
-#define  XHCI_USB3_PORTSC_WCE	(1 << 25)	/* Wake on Connect */
-#define  XHCI_USB3_PORTSC_WDE	(1 << 26)	/* Wake on Disconnect */
-#define  XHCI_USB3_PORTSC_WOE	(1 << 27)	/* Wake on Overcurrent */
-#define  XHCI_USB3_PORTSC_WRC	(1 << 19)	/* Warm Reset Complete */
-#define  XHCI_USB3_PORTSC_LWS  	(1 << 16)	/* Link Write Strobe */
-#define  XHCI_USB3_PORTSC_PED 	(1 << 1)	/* Port Enabled/Disabled */
-#define  XHCI_USB3_PORTSC_WPR	(1 << 31)	/* Warm Port Reset */
-#define  XHCI_USB3_PORTSC_PLS	(0xf << 5)	/* Port Link State */
-#define   XHCI_PLSR_DISABLED	(4 << 5)	/* Port is disabled */
-#define   XHCI_PLSR_RXDETECT	(5 << 5)	/* Port is disconnected */
-#define   XHCI_PLSR_POLLING	(7 << 5)	/* Port is polling */
-#define   XHCI_PLSW_ENABLE	(5 << 5)	/* Transition from disabled */
 
 /* PCI Configuration Space (D31:F3): SMBus */
 #define PCH_SMBUS_DEV		PCI_DEV(0, 0x1f, 3)
