536c34c2d92178f4e62b8ca7cfffceaf80a305f6
Marc Jones <marc.jones@se-eng.com>
2014-05-01 14:54:11 -0600

    SPI: Add Eon EN25S64 support.
    BUG=chrome-os-partner:25907
    BRANCH=baytrail(rambi)
    TEST=Read and write MRC and ELOG on Glimmer with Eon device.
    
    Original-Change-Id: I69b0a330acbff97ebb8dc3ce3e37f7452433b5dc
    Original-Signed-off-by: Marc Jones <marc.jones@se-eng.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/197882
    Original-Reviewed-by: David Hendricks <dhendrix@chromium.org>
    (cherry picked from commit 2bd9b4250fe44c33a15f8615de4034cfff4cf3b5)
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>
    
    Change-Id: If883ff6eb14dd49a06f57a01ca61661854ded78d
    Reviewed-on: https://chromium-review.googlesource.com/198324
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Commit-Queue: Marc Jones <marc.jones@se-eng.com>
    Tested-by: Marc Jones <marc.jones@se-eng.com>

--- src/drivers/spi/eon.c
+++ src/drivers/spi/eon.c
@@ -24,6 +24,7 @@
 #define CMD_EN25_RES		0xab	/* Release from DP, and Read Signature */
 
 #define EON_ID_EN25Q128		0x3018
+#define EON_ID_EN25S64			0x3817
 
 struct eon_spi_flash_params {
 	u16 id;
@@ -53,6 +54,14 @@ static const struct eon_spi_flash_params eon_spi_flash_table[] = {
 		.sectors_per_block = 16,
 		.nr_sectors = 4096,
 		.name = "EN25Q128",
+	},
+	{
+		.id = EON_ID_EN25S64,
+		.page_size = 256,
+		.pages_per_sector = 16,
+		.sectors_per_block = 16,
+		.nr_sectors = 2048,
+		.name = "EN25S64",
 	},
 };
 
