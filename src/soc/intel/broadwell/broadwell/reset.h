6d1efb94bd39bcd6f7e3e0de2f3299a384b109ef
Duncan Laurie <dlaurie@chromium.org>
2014-05-01 10:51:51 -0700

    broadwell: Add reset_system function and header
    Add a reset_system() function that is used in romstage and ramstage
    code and a broadwell/reset.h header for it.
    
    BUG=chrome-os-partner:28234
    TEST=None
    
    Change-Id: I4f96f0506a1147382b46b3540ffd5f520894fdc5
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/198915
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/broadwell/broadwell/reset.h
+++ src/soc/intel/broadwell/broadwell/reset.h
@@ -1,7 +1,6 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2008-2009 coresystems GmbH
  * Copyright (C) 2014 Google Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -18,34 +17,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <arch/hlt.h>
-#include <arch/io.h>
-#include <reset.h>
-#include <broadwell/reset.h>
+#ifndef _BROADWELL_RESET_H_
+#define _BROADWELL_RESET_H_
 
-/*
- * Soft reset (INIT# to cpu) - write 0x1 to I/O 0x92
- * Soft reset (INIT# to cpu)- write 0x4 to I/0 0xcf9
- * Cold reset (S0->S5->S0) - write 0xe to I/0 0xcf9
- * Warm reset (PLTRST# assertion) - write 0x6 to I/O 0xcf9
- * Global reset (S0->S5->S0 with ME reset) - write 0x6 or 0xe to 0xcf9 but
- * with ETR[20] set.
- */
+void reset_system(void);
 
-void soft_reset(void)
-{
-        outb(0x04, 0xcf9);
-}
-
-void hard_reset(void)
-{
-        outb(0x06, 0xcf9);
-}
-
-void reset_system(void)
-{
-	hard_reset();
-	while (1) {
-		hlt();
-	}
-}
+#endif
