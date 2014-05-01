ffa5743f74551bd48aa7e5445ce7cd9dc7b07ce8
Duncan Laurie <dlaurie@chromium.org>
2014-05-01 16:04:16 -0700

    broadwell: Clean up SATA ramstage driver
    - Remove the IDE and SATA (plan) options since they are not supported
    by the chipset.
    - Remove the LynxPoint-LP specific checks since only LP variant
    is supported.
    - Add code to static power gate the unused ports.
    
    BUG=chrome-os-partner:28234
    TEST=None
    
    Change-Id: Ic161abb167cc406163b12c643685a6958382c4a9
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/199367
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/broadwell/sata.c
+++ src/soc/intel/broadwell/sata.c
@@ -24,10 +24,11 @@
 #include <device/pci.h>
 #include <device/pci_ids.h>
 #include <delay.h>
-#include "pch.h"
-
-typedef struct southbridge_intel_lynxpoint_config config_t;
+#include <broadwell/iobp.h>
 #include <broadwell/ramstage.h>
+#include <broadwell/rcba.h>
+#include <broadwell/sata.h>
+#include <chip.h>
 
 static inline u32 sir_read(struct device *dev, int idx)
 {
@@ -43,191 +44,105 @@ static inline void sir_write(struct device *dev, int idx, u32 value)
 
 static void sata_init(struct device *dev)
 {
-	u32 reg32;
-	u16 reg16;
-	/* Get the chip configuration */
 	config_t *config = dev->chip_info;
+	u32 reg32, abar;
+	u16 reg16;
 
-	printk(BIOS_DEBUG, "SATA: Initializing...\n");
-
-	if (config == NULL) {
-		printk(BIOS_ERR, "SATA: ERROR: Device not in devicetree.cb!\n");
-		return;
-	}
-
-	/* SATA configuration */
+	printk(BIOS_DEBUG, "SATA: Initializing controller in AHCI mode.\n");
 
 	/* Enable BARs */
 	pci_write_config16(dev, PCI_COMMAND, 0x0007);
 
-	if (config->ide_legacy_combined) {
-		printk(BIOS_DEBUG, "SATA: Controller in combined mode.\n");
+	/* Set Interrupt Line */
+	/* Interrupt Pin is set by D31IP.PIP */
+	pci_write_config8(dev, PCI_INTERRUPT_LINE, 0x0a);
 
-		/* No AHCI: clear AHCI base */
-		pci_write_config32(dev, 0x24, 0x00000000);
-		/* And without AHCI BAR no memory decoding */
-		reg16 = pci_read_config16(dev, PCI_COMMAND);
-		reg16 &= ~PCI_COMMAND_MEMORY;
-		pci_write_config16(dev, PCI_COMMAND, reg16);
+	/* Set timings */
+	pci_write_config16(dev, IDE_TIM_PRI, IDE_DECODE_ENABLE |
+			   IDE_ISP_3_CLOCKS | IDE_RCT_1_CLOCKS |
+			   IDE_PPE0 | IDE_IE0 | IDE_TIME0);
+	pci_write_config16(dev, IDE_TIM_SEC, IDE_DECODE_ENABLE |
+			   IDE_ISP_5_CLOCKS | IDE_RCT_4_CLOCKS);
 
-		pci_write_config8(dev, 0x09, 0x80);
+	/* Sync DMA */
+	pci_write_config16(dev, IDE_SDMA_CNT, IDE_PSDE0);
+	pci_write_config16(dev, IDE_SDMA_TIM, 0x0001);
 
-		/* Set timings */
-		pci_write_config16(dev, IDE_TIM_PRI, IDE_DECODE_ENABLE |
-				IDE_ISP_5_CLOCKS | IDE_RCT_4_CLOCKS);
-		pci_write_config16(dev, IDE_TIM_SEC, IDE_DECODE_ENABLE |
-				IDE_ISP_3_CLOCKS | IDE_RCT_1_CLOCKS |
-				IDE_PPE0 | IDE_IE0 | IDE_TIME0);
+	/* Set IDE I/O Configuration */
+	reg32 = SIG_MODE_PRI_NORMAL | FAST_PCB1 | FAST_PCB0 | PCB1 | PCB0;
+	pci_write_config32(dev, IDE_CONFIG, reg32);
 
-		/* Sync DMA */
-		pci_write_config16(dev, IDE_SDMA_CNT, IDE_SSDE0);
-		pci_write_config16(dev, IDE_SDMA_TIM, 0x0200);
+	/* for AHCI, Port Enable is managed in memory mapped space */
+	reg16 = pci_read_config16(dev, 0x92);
+	reg16 &= ~0x3f;
+	reg16 |= 0x8000 | config->sata_port_map;
+	pci_write_config16(dev, 0x92, reg16);
+	udelay(2);
 
-		/* Set IDE I/O Configuration */
-		reg32 = SIG_MODE_PRI_NORMAL | FAST_PCB1 | FAST_PCB0 | PCB1 | PCB0;
-		pci_write_config32(dev, IDE_CONFIG, reg32);
+	/* Setup register 98h */
+	reg32 = pci_read_config16(dev, 0x98);
+	reg32 |= 1 << 19;    /* BWG step 6 */
+	reg32 |= 1 << 22;    /* BWG step 5 */
+	reg32 &= ~(0x3f << 7);
+	reg32 |= 0x04 << 7;  /* BWG step 7 */
+	reg32 |= 1 << 20;    /* BWG step 8 */
+	reg32 &= ~(0x03 << 5);
+	reg32 |= 1 << 5;     /* BWG step 9 */
+	reg32 |= 1 << 18;    /* BWG step 10 */
+	reg32 |= 1 << 29;    /* BWG step 11 */
+	reg32 &= ~((1 << 31) | (1 << 30));
+	reg32 |= 1 << 23;
+	reg32 |= 1 << 24; /* Disable listen mode (hotplug) */
+	pci_write_config32(dev, 0x98, reg32);
 
-		/* Port enable */
-		reg16 = pci_read_config16(dev, 0x92);
-		reg16 &= ~0x3f;
-		reg16 |= config->sata_port_map;
-		pci_write_config16(dev, 0x92, reg16);
+	/* Setup register 9Ch */
+	reg16 = 0;           /* Disable alternate ID */
+	reg16 = 1 << 5;      /* BWG step 12 */
+	pci_write_config16(dev, 0x9c, reg16);
 
-		/* SATA Initialization register */
-		pci_write_config32(dev, 0x94,
-			   ((config->sata_port_map ^ 0x3f) << 24) | 0x183);
-	} else if(config->sata_ahci) {
-		u32 abar;
+	/* SATA Initialization register */
+	reg32 = 0x183;
+	reg32 |= (config->sata_port_map ^ 0x3f) << 24;
+	reg32 |= (config->sata_devslp_mux & 1) << 15;
+	pci_write_config32(dev, 0x94, reg32);
 
-		printk(BIOS_DEBUG, "SATA: Controller in AHCI mode.\n");
+	/* Initialize AHCI memory-mapped space */
+	abar = pci_read_config32(dev, PCI_BASE_ADDRESS_5);
+	printk(BIOS_DEBUG, "ABAR: %08X\n", abar);
 
-		/* Set Interrupt Line */
-		/* Interrupt Pin is set by D31IP.PIP */
-		pci_write_config8(dev, INTR_LN, 0x0a);
+	/* CAP (HBA Capabilities) : enable power management */
+	reg32 = read32(abar + 0x00);
+	reg32 |= 0x0c006000;  // set PSC+SSC+SALP+SSS
+	reg32 &= ~0x00020060; // clear SXS+EMS+PMS
+	reg32 |= (1 << 18);   // SAM: SATA AHCI MODE ONLY
+	write32(abar + 0x00, reg32);
 
-		/* Set timings */
-		pci_write_config16(dev, IDE_TIM_PRI, IDE_DECODE_ENABLE |
-				IDE_ISP_3_CLOCKS | IDE_RCT_1_CLOCKS |
-				IDE_PPE0 | IDE_IE0 | IDE_TIME0);
-		pci_write_config16(dev, IDE_TIM_SEC, IDE_DECODE_ENABLE |
-				IDE_ISP_5_CLOCKS | IDE_RCT_4_CLOCKS);
+	/* PI (Ports implemented) */
+	write32(abar + 0x0c, config->sata_port_map);
+	(void) read32(abar + 0x0c); /* Read back 1 */
+	(void) read32(abar + 0x0c); /* Read back 2 */
 
-		/* Sync DMA */
-		pci_write_config16(dev, IDE_SDMA_CNT, IDE_PSDE0);
-		pci_write_config16(dev, IDE_SDMA_TIM, 0x0001);
+	/* CAP2 (HBA Capabilities Extended)*/
+	reg32 = read32(abar + 0x24);
 
-		/* Set IDE I/O Configuration */
-		reg32 = SIG_MODE_PRI_NORMAL | FAST_PCB1 | FAST_PCB0 | PCB1 | PCB0;
-		pci_write_config32(dev, IDE_CONFIG, reg32);
+	/*
+	 * Static Power Gating for unused ports
+	 */
+	reg32 = RCBA32(0x3a84);
+	/* Port 3 and 2 disabled */
+	if ((config->sata_port_map & ((1 << 3)|(1 << 2))) == 0)
+		reg32 |= (1 << 24) | (1 << 26);
+	/* Port 1 and 0 disabled */
+	if ((config->sata_port_map & ((1 << 1)|(1 << 0))) == 0)
+		reg32 |= (1 << 20) | (1 << 18);
+	RCBA32(0x3a84) = reg32;
 
-		/* for AHCI, Port Enable is managed in memory mapped space */
-		reg16 = pci_read_config16(dev, 0x92);
-		reg16 &= ~0x3f;
-		reg16 |= 0x8000 | config->sata_port_map;
-		pci_write_config16(dev, 0x92, reg16);
-		udelay(2);
-
-		/* Setup register 98h */
-		reg32 = pci_read_config16(dev, 0x98);
-		reg32 |= 1 << 19;    /* BWG step 6 */
-		reg32 |= 1 << 22;    /* BWG step 5 */
-		reg32 &= ~(0x3f << 7);
-		reg32 |= 0x04 << 7;  /* BWG step 7 */
-		reg32 |= 1 << 20;    /* BWG step 8 */
-		reg32 &= ~(0x03 << 5);
-		reg32 |= 1 << 5;     /* BWG step 9 */
-		reg32 |= 1 << 18;    /* BWG step 10 */
-		reg32 |= 1 << 29;    /* BWG step 11 */
-		if (pch_is_lp()) {
-			reg32 &= ~((1 << 31) | (1 << 30));
-			reg32 |= 1 << 23;
-			reg32 |= 1 << 24; /* Disable listen mode (hotplug) */
-		}
-		pci_write_config32(dev, 0x98, reg32);
-
-		/* Setup register 9Ch */
-		reg16 = 0;           /* Disable alternate ID */
-		reg16 = 1 << 5;      /* BWG step 12 */
-		pci_write_config16(dev, 0x9c, reg16);
-
-		/* SATA Initialization register */
-		reg32 = 0x183;
-		reg32 |= (config->sata_port_map ^ 0x3f) << 24;
-		reg32 |= (config->sata_devslp_mux & 1) << 15;
-		pci_write_config32(dev, 0x94, reg32);
-
-		/* Initialize AHCI memory-mapped space */
-		abar = pci_read_config32(dev, PCI_BASE_ADDRESS_5);
-		printk(BIOS_DEBUG, "ABAR: %08X\n", abar);
-		/* CAP (HBA Capabilities) : enable power management */
-		reg32 = read32(abar + 0x00);
-		reg32 |= 0x0c006000;  // set PSC+SSC+SALP+SSS
-		reg32 &= ~0x00020060; // clear SXS+EMS+PMS
-		if (pch_is_lp())
-			reg32 |= (1 << 18);   // SAM: SATA AHCI MODE ONLY
-		write32(abar + 0x00, reg32);
-		/* PI (Ports implemented) */
-		write32(abar + 0x0c, config->sata_port_map);
-		(void) read32(abar + 0x0c); /* Read back 1 */
-		(void) read32(abar + 0x0c); /* Read back 2 */
-		/* CAP2 (HBA Capabilities Extended)*/
-		reg32 = read32(abar + 0x24);
-		/* Enable DEVSLP */
-		if (pch_is_lp()) {
-			if (config->sata_devslp_disable)
-				reg32 &= ~(1 << 3);
-			else
-				reg32 |= (1 << 5)|(1 << 4)|(1 << 3)|(1 << 2);
-		} else {
-			reg32 &= ~0x00000002;
-		}
-		write32(abar + 0x24, reg32);
-	} else {
-		printk(BIOS_DEBUG, "SATA: Controller in plain mode.\n");
-
-		/* No AHCI: clear AHCI base */
-		pci_write_config32(dev, 0x24, 0x00000000);
-
-		/* And without AHCI BAR no memory decoding */
-		reg16 = pci_read_config16(dev, PCI_COMMAND);
-		reg16 &= ~PCI_COMMAND_MEMORY;
-		pci_write_config16(dev, PCI_COMMAND, reg16);
-
-		/* Native mode capable on both primary and secondary (0xa)
-		 * or'ed with enabled (0x50) = 0xf
-		 */
-		pci_write_config8(dev, 0x09, 0x8f);
-
-		/* Set Interrupt Line */
-		/* Interrupt Pin is set by D31IP.PIP */
-		pci_write_config8(dev, INTR_LN, 0xff);
-
-		/* Set timings */
-		pci_write_config16(dev, IDE_TIM_PRI, IDE_DECODE_ENABLE |
-				IDE_ISP_3_CLOCKS | IDE_RCT_1_CLOCKS |
-				IDE_PPE0 | IDE_IE0 | IDE_TIME0);
-		pci_write_config16(dev, IDE_TIM_SEC, IDE_DECODE_ENABLE |
-				IDE_SITRE | IDE_ISP_3_CLOCKS |
-				IDE_RCT_1_CLOCKS | IDE_IE0 | IDE_TIME0);
-
-		/* Sync DMA */
-		pci_write_config16(dev, IDE_SDMA_CNT, IDE_SSDE0 | IDE_PSDE0);
-		pci_write_config16(dev, IDE_SDMA_TIM, 0x0201);
-
-		/* Set IDE I/O Configuration */
-		reg32 = SIG_MODE_PRI_NORMAL | FAST_PCB1 | FAST_PCB0 | PCB1 | PCB0;
-		pci_write_config32(dev, IDE_CONFIG, reg32);
-
-		/* Port enable */
-		reg16 = pci_read_config16(dev, 0x92);
-		reg16 &= ~0x3f;
-		reg16 |= config->sata_port_map;
-		pci_write_config16(dev, 0x92, reg16);
-
-		/* SATA Initialization register */
-		pci_write_config32(dev, 0x94,
-			   ((config->sata_port_map ^ 0x3f) << 24) | 0x183);
-	}
+	/* Enable DEVSLP */
+	if (config->sata_devslp_disable)
+		reg32 &= ~(1 << 3);
+	else
+		reg32 |= (1 << 5)|(1 << 4)|(1 << 3)|(1 << 2);
+	write32(abar + 0x24, reg32);
 
 	/* Set Gen3 Transmitter settings if needed */
 	if (config->sata_port0_gen3_tx)
@@ -263,14 +178,12 @@ static void sata_init(struct device *dev)
 				<< SATA_DTLE_EDGE_SHIFT);
 	}
 
-	/* Additional Programming Requirements */
-	/* Power Optimizer */
+	/*
+	 * Additional Programming Requirements for Power Optimizer
+	 */
 
 	/* Step 1 */
-	if (pch_is_lp())
-		sir_write(dev, 0x64, 0x883c9003);
-	else
-		sir_write(dev, 0x64, 0x883c9001);
+	sir_write(dev, 0x64, 0x883c9003);
 
 	/* Step 2: SIR 68h[15:0] = 880Ah */
 	reg32 = sir_read(dev, 0x68);
@@ -295,10 +208,8 @@ static void sata_init(struct device *dev)
 
 	/* Clock Gating */
 	sir_write(dev, 0x70, 0x3f00bf1f);
-	if (pch_is_lp()) {
-		sir_write(dev, 0x54, 0xcf000f0f);
-		sir_write(dev, 0x58, 0x00190000);
-	}
+	sir_write(dev, 0x54, 0xcf000f0f);
+	sir_write(dev, 0x58, 0x00190000);
 
 	reg32 = pci_read_config32(dev, 0x300);
 	reg32 |= (1 << 17) | (1 << 16);
@@ -306,21 +217,15 @@ static void sata_init(struct device *dev)
 	pci_write_config32(dev, 0x300, reg32);
 }
 
+/*
+ * Set SATA controller mode early so the resource allocator can
+ * properly assign IO/Memory resources for the controller.
+ */
 static void sata_enable(device_t dev)
 {
 	/* Get the chip configuration */
 	config_t *config = dev->chip_info;
-	u16 map = 0;
-
-	if (!config)
-		return;
-
-	/*
-	 * Set SATA controller mode early so the resource allocator can
-	 * properly assign IO/Memory resources for the controller.
-	 */
-	if (config->sata_ahci)
-		map = 0x0060;
+	u16 map = 0x0060;
 
 	map |= (config->sata_port_map ^ 0x3f) << 8;
 
