56f2f3bff0dfdd1909670b987969f54a75b88226
david <david_wu@quantatw.com>
2014-07-28 18:01:28 +0800

    haswell: Update microcode revision
    CPUID 306C3 Haswell MOB C-0 microcode to 1ch
    CPUID 40651 Haswell ULT C-0 microcode to 1ch
    
    BUG=none
    BRANCH=peppy
    TEST=manual: build and boot on peppy and check microcode revision
    localhost ~ # grep microcode /proc/cpuinfo
    microcode       : 0x1c
    microcode       : 0x1c
    
    Change-Id: I1a4c21c81c5dc08dfbb296e133f5677887bd7877
    Signed-off-by: David Wu <David_Wu@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/210031
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Commit-Queue: David Wu <david_wu@quantatw.com>
    Tested-by: David Wu <david_wu@quantatw.com>

--- src/cpu/intel/haswell/microcode_blob.h
+++ src/cpu/intel/haswell/microcode_blob.h
@@ -19,11 +19,11 @@
 
 #if CONFIG_INTEL_LYNXPOINT_LP
 #include "microcode-M7240650_ffff000a.h"
-#include "microcode-M7240651_00000017.h"
+#include "microcode-M7240651_0000001c.h"
 #else
 #include "microcode-M32306c1_ffff000d.h"
 #include "microcode-M32306c2_ffff0003.h"
-#include "microcode-M32306c3_00000017.h"
+#include "microcode-M32306c3_0000001c.h"
 #include "microcode-M3240660_ffff000b.h"
 #endif
 	/*  Dummy terminator  */
