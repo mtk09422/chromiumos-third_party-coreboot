6dad573c8689b79bb4aa615811a10f44e7d8c809
Aaron Durbin <adurbin@chromium.org>
2014-08-06 15:27:12 -0500

    tegra132: move common bootblock init into SoC code
    The current 2 boards were setting up clocks and enabling
    peripherals that apply to the SoC generically. Therefore,
    move the common pieces into the SoC code.
    
    BUG=chrome-os-partner:31105
    BRANCH=None
    TEST=Built and booted through depthcharge on ryu.
    
    Change-Id: I6df1813f88362b8beaf1a716f4f92e42e4b73406
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/211191
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Tom Warren <twarren@nvidia.com>

--- src/soc/nvidia/tegra132/bootblock.c
+++ src/soc/nvidia/tegra132/bootblock.c
@@ -21,6 +21,7 @@
 #include <arch/hlt.h>
 #include <cbfs.h>
 #include <console/console.h>
+#include <soc/addressmap.h>
 #include <soc/bootblock.h>
 #include <soc/clock.h>
 #include <soc/nvidia/tegra/apbmisc.h>
@@ -33,6 +34,8 @@ void __attribute__((weak)) bootblock_mainboard_early_init(void)
 	/* Empty default implementation. */
 }
 
+static struct clk_rst_ctlr *clk_rst = (void *)TEGRA_CLK_RST_BASE;
+
 void main(void)
 {
 	void *entry;
@@ -44,6 +47,14 @@ void main(void)
 	enable_jtag();
 
 	clock_early_uart();
+
+	/* Configure mselect clock. */
+	clock_configure_source(mselect, PLLP, 102000);
+
+	/* Enable AVP cache, timer, APB dma, and mselect blocks.  */
+	clock_enable_clear_reset(CLK_L_CACHE2 | CLK_L_TMR,
+				 CLK_H_APBDMA,
+				 0, CLK_V_MSELECT, 0, 0);
 
 	bootblock_mainboard_early_init();
 
