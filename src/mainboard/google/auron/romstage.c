f4e9c9b338b3d43ea0474c1e079516964dc61de0
Shawn Nematbakhsh <shawnn@chromium.org>
2014-08-21 14:56:09 -0700

    auron: Move PEI data structure init to separate file
    This needs to be executed in both romstage and ramstage
    for the different PEI binary stages.
    
    It uses the broadwell interface now instead of haswell.
    
    Auron clone of Samus commit 89f98a27ea561ec63e716b1f6446d92822a6a5de.
    
    BUG=chrome-os-partner:31286
    TEST=Compile only.
    BRANCH=None.
    
    Change-Id: I57f4d6f17c589e1b42ee20e6824c77eb382b44af
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/213956

--- src/mainboard/google/auron/romstage.c
+++ src/mainboard/google/auron/romstage.c
@@ -73,59 +73,7 @@ const struct rcba_config_instruction rcba_config[] = {
 
 void mainboard_romstage_entry(unsigned long bist)
 {
-	struct pei_data pei_data = {
-		pei_version: PEI_VERSION,
-		mchbar: DEFAULT_MCHBAR,
-		dmibar: DEFAULT_DMIBAR,
-		epbar: DEFAULT_EPBAR,
-		pciexbar: DEFAULT_PCIEXBAR,
-		smbusbar: SMBUS_IO_BASE,
-		wdbbar: 0x4000000,
-		wdbsize: 0x1000,
-		hpet_address: HPET_ADDR,
-		rcba: DEFAULT_RCBA,
-		pmbase: DEFAULT_PMBASE,
-		gpiobase: DEFAULT_GPIOBASE,
-		temp_mmio_base: 0xfed08000,
-		system_type: 5, /* ULT */
-		tseg_size: CONFIG_SMM_TSEG_SIZE,
-		spd_addresses: { 0xff, 0x00, 0xff, 0x00 },
-		ec_present: 1,
-		// 0 = leave channel enabled
-		// 1 = disable dimm 0 on channel
-		// 2 = disable dimm 1 on channel
-		// 3 = disable dimm 0+1 on channel
-		dimm_channel0_disabled: 2,
-		dimm_channel1_disabled: 2,
-		max_ddr3_freq: 1600,
-		usb_xhci_on_resume: 1,
-		usb2_ports: {
-			/* Length, Enable, OCn#, Location */
-			{ 0x0150, 1, USB_OC_PIN_SKIP, /* P0: LTE */
-			  USB_PORT_MINI_PCIE },
-			{ 0x0040, 1, 0,               /* P1: Port A, CN10 */
-			  USB_PORT_BACK_PANEL },
-			{ 0x0080, 1, USB_OC_PIN_SKIP, /* P2: CCD */
-			  USB_PORT_INTERNAL },
-			{ 0x0040, 1, USB_OC_PIN_SKIP, /* P3: BT */
-			  USB_PORT_MINI_PCIE },
-			{ 0x0040, 1, 2,               /* P4: Port B, CN6  */
-			  USB_PORT_BACK_PANEL },
-			{ 0x0000, 0, USB_OC_PIN_SKIP, /* P5: EMPTY */
-			  USB_PORT_SKIP },
-			{ 0x0150, 1, USB_OC_PIN_SKIP, /* P6: SD Card */
-			  USB_PORT_FLEX },
-			{ 0x0000, 0, USB_OC_PIN_SKIP, /* P7: EMPTY */
-			  USB_PORT_SKIP },
-		},
-		usb3_ports: {
-			/* Enable, OCn# */
-			{ 1, 0               }, /* P1; Port A, CN6 */
-			{ 0, USB_OC_PIN_SKIP }, /* P2; */
-			{ 0, USB_OC_PIN_SKIP }, /* P3; */
-			{ 0, USB_OC_PIN_SKIP }, /* P4; */
-		},
-	};
+	struct pei_data pei_data;
 
 	struct romstage_params romstage_params = {
 		.pei_data = &pei_data,
@@ -134,6 +82,9 @@ void mainboard_romstage_entry(unsigned long bist)
 		.bist = bist,
 	};
 
+	/* Fill out PEI data */
+	memset(&pei_data, 0, sizeof(pei_data));
+	mainboard_fill_pei_data(&pei_data);
 	mainboard_fill_spd_data(&pei_data);
 
 	/* Call into the real romstage main with this board's attributes. */
