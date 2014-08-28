0da45307be8729ca51e0b7e9eb239ffd78d9f8ed
Kane Chen <kane.chen@intel.com>
2014-08-27 23:04:10 -0700

    auron: Move SPD related files/information to spd directory
    Auron port of Samus commit f40e447cee84ebd04ab8a57250d0f56f508d52f2
    
    BUG=chrome-os-partner:31286
    TEST=compile ok and make sure the spd index is right on auron
         boot to OS
    BRANCH=None
    
    Change-Id: Idf8f58dc48ff7dd2481177aa377628cfa032b699
    Signed-off-by: Kane Chen <kane.chen@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/214820
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- src/mainboard/google/auron/spd/spd.h
+++ src/mainboard/google/auron/spd/spd.h
@@ -22,6 +22,16 @@
 
 #define SPD_LEN			256
 
+#define SPD_DRAM_TYPE		2
+#define  SPD_DRAM_DDR3		0x0b
+#define  SPD_DRAM_LPDDR3	0xf1
+#define SPD_DENSITY_BANKS	4
+#define SPD_ADDRESSING		5
+#define SPD_ORGANIZATION	7
+#define SPD_BUS_DEV_WIDTH	8
+#define SPD_PART_OFF		128
+#define  SPD_PART_LEN		18
+
 /* Auron board memory configuration GPIOs */
 #define SPD_GPIO_BIT0		13
 #define SPD_GPIO_BIT1		9
