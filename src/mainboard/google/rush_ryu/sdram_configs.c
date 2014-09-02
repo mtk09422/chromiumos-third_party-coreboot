a45e7788dd78697ac5f48b6cc64108ca0e4912dd
Tom Warren <twarren@nvidia.com>
2014-09-02 13:41:54 -0700

    ryu: Add 4 LPDDR3 SDRAM BCTs
    These are used by the LPDDR3 code in sdram.c.
    
    Based on the schematic and email, I've filled in 4 slots
    in sdram_configs.c. My A44 returns RAMCODE 0 (using only bits
    1:0) for Samsung SDRAM. I haven't tested the other 2 types of
    RAM (Hynix and Micron). The 4th slot is a fallback slow Micron
    config.
    
    BUG=chrome-os-partner:29921
    BUG=chrome-os-partner:31031
    BRANCH=None
    TEST=Built for rush and rush_ryu.
    
    Change-Id: Ib7e8b814eb6dadb9b366536721876a3eeba0d2c0
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/216000
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/mainboard/google/rush_ryu/sdram_configs.c
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2014 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <console/console.h>
+#include <soc/sdram_configs.h>
+#include <stdlib.h>
+
+static const struct sdram_params sdram_configs[] = {
+#include "bct/sdram-samsung-4GB-924.inc"		/* ram_code = 00 */
+#include "bct/sdram-hynix-4GB-924.inc"			/* ram_code = 01 */
+#include "bct/sdram-micron-4GB-924.inc"			/* ram_code = 10 */
+#include "bct/sdram-micron-4GB-528.inc"			/* ram_code = 11 */
+};
+
+const struct sdram_params *get_sdram_config()
+{
+	uint32_t ramcode = sdram_get_ram_code();
+
+	/*
+	 * If we need to apply some special hacks to RAMCODE mapping (ex, by
+	 * board_id), do that now.
+	 */
+
+	ramcode &= 0x3;		/* Only bits 1:0 used on Ryu */
+	printk(BIOS_SPEW, "%s: RAMCODE=%d\n", __func__, ramcode);
+
+	if (ramcode >= ARRAY_SIZE(sdram_configs) ||
+	    sdram_configs[ramcode].MemoryType == NvBootMemoryType_Unused) {
+		die("Invalid RAMCODE.");
+	}
+
+	return &sdram_configs[ramcode];
+}
