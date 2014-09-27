4b9b3c2f8b7c6cd189cb8f239508431ee08ebc52
huang lin <hl@rock-chips.com>
2014-09-27 12:02:27 +0800

    rockchip: support i2c clock setting
    BUG=None
    TEST=Boot Veyron Pinky and measure i2c clock frequency
    
    Change-Id: I04d9fa75a05280885f083a828f78cf55811ca97d
    Signed-off-by: huang lin <hl@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/219660
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Julius Werner <jwerner@chromium.org>

--- src/soc/rockchip/rk3288/clock.c
+++ src/soc/rockchip/rk3288/clock.c
@@ -157,8 +157,8 @@ static const struct pll_div cpll_init_cfg = PLL_DIVISORS(CPLL_HZ, 2, 4);
  * aclk_periph =
  * periph_clk_src / (peri_aclk_div_con + 1)
  */
+#define PERI_ACLK_DIV_SHIFT	(0x0)
 #define PERI_ACLK_DIV_MSK	(0x1F)
-#define PERI_ACLK_DIV_SHIFT	(0)
 
 /*******************CLKSEL37 BITS***************************/
 #define L2_DIV_MSK	(0x7)
