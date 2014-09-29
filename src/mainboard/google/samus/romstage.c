0e3ebcb8659c92874d3ca89fa3a6795c9b6eebfa
Duncan Laurie <dlaurie@chromium.org>
2014-09-29 08:35:29 -0700

    samus: Fix and clean up GPIOs and EC info/events
    - Define specific GPIOs in gpio.h instaed of smihandler.c
    - Add battery status event to SCI list
    - Remove old proto board version defines and SPD index usage
    - Do not disable cmd_pwr training now that it works on EVT board
    
    BUG=chrome-os-partner:32196,chrome-os-partner:29117
    BRANCH=samus
    TEST=build and boot on samus
    
    Change-Id: I53cf8d80ed7f675c10fa04e8fe8b879a4af9b21f
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/220321
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/google/samus/romstage.c
+++ src/mainboard/google/samus/romstage.c
@@ -56,14 +56,6 @@ void mainboard_romstage_entry(struct romstage_params *rp)
 	mainboard_fill_spd_data(&pei_data);
 	rp->pei_data = &pei_data;
 
-	/*
-	 * http://crosbug.com/p/29117
-	 * Disable use of PEI saved data to work around memory issues.
-	 */
-	if (cpu_family_model() == BROADWELL_FAMILY_ULT) {
-		pei_data.disable_cmd_pwr = 1;
-	}
-
 	/* Initliaze memory */
 	romstage_common(rp);
 
