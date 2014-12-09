a7ab7225e3419a0fd93894dbb9a959390f29945b
Jimmy Zhang <jimmzhang@nvidia.com>
2014-12-09 15:20:20 -0800

    ryu: display: Split dc functions from dsi display code
    dc supporting functions can be used for other than dsi display
    interfaces. This change is preparing for supporting sor display
    interface.
    
    BUG=chrome-os-partner:34336
    BRANCH=none
    TEST=build ryu and test dev/rec mode, also build rush ok
    
    Change-Id: Id14cbd89457cb91c23526927a432f4eb7cc6291b
    Signed-off-by: Jimmy Zhang <jimmzhang@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/234270
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/nvidia/tegra/dc.h
+++ src/soc/nvidia/tegra/dc.h
@@ -25,6 +25,7 @@
 #ifndef __SOC_NVIDIA_TEGRA_DC_H
 #define __SOC_NVIDIA_TEGRA_DC_H
 #include <stddef.h>
+#include <device/device.h>
 
 /* Register definitions for the Tegra display controller */
 
@@ -503,7 +504,9 @@ struct tegra_dc {
 unsigned long READL(void * p);
 void WRITEL(unsigned long value, void * p);
 
+#ifndef __PRE_RAM__
 void display_startup(device_t dev);
+#endif
 void dp_init(void * _config);
 void dp_enable(void * _dp);
 unsigned int fb_base_mb(void);
