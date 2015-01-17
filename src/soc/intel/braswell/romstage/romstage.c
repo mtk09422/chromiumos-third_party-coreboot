5f69b95037e7d49381cba2ce9bf2c551212fbe13
Lee Leahy <leroy.p.leahy@intel.com>
2015-01-16 22:14:24 -0800

    Braswell: MemoryInit support
    Include the necessary files to support the FSP MemoryInit call.
    
    BRANCH=none
    BUG=None
    TEST=Wait for Strago MemoryInit patch
    
    Change-Id: I07db1d36fd7de3f576e18dd56df565e427f7fecf
    Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/241770
    Reviewed-by: Erik C Bjorge <erik.c.bjorge@intel.com>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/soc/intel/braswell/romstage/romstage.c
+++ src/soc/intel/braswell/romstage/romstage.c
@@ -18,6 +18,11 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <arch/early_variables.h>
+#include <arch/cpu.h>
+#include <arch/hlt.h>
+#include <soc/intel/common/mrc_cache.h>
+
 #include <soc/gpio.h>
 #include <soc/iomap.h>
 #include <soc/lpc.h>
@@ -72,6 +77,67 @@ void program_base_addresses(void)
 	pci_write_config32(lpc_dev, GBASE, reg);
 }
 
+void spi_init(void)
+{
+	const unsigned long scs = SPI_BASE_ADDRESS + SCS;
+	const unsigned long bcr = SPI_BASE_ADDRESS + BCR;
+	uint32_t reg;
+
+	/* Disable generating SMI when setting WPD bit. */
+	write32(scs, read32(scs) & ~SMIWPEN);
+	/*
+	 * Enable caching and prefetching in the SPI controller. Disable
+	 * the SMM-only BIOS write and set WPD bit.
+	 */
+	reg = (read32(bcr) & ~SRC_MASK) | SRC_CACHE_PREFETCH | BCR_WPD;
+	reg &= ~EISS;
+	write32(bcr, reg);
+}
+
+static struct chipset_power_state power_state CAR_GLOBAL;
+
+struct chipset_power_state *fill_power_state(void)
+{
+	struct chipset_power_state *ps = car_get_var_ptr(&power_state);
+
+	ps->pm1_sts = inw(ACPI_BASE_ADDRESS + PM1_STS);
+	ps->pm1_en = inw(ACPI_BASE_ADDRESS + PM1_EN);
+	ps->pm1_cnt = inl(ACPI_BASE_ADDRESS + PM1_CNT);
+	ps->gpe0_sts = inl(ACPI_BASE_ADDRESS + GPE0_STS);
+	ps->gpe0_en = inl(ACPI_BASE_ADDRESS + GPE0_EN);
+	ps->tco_sts = inl(ACPI_BASE_ADDRESS + TCO_STS);
+	ps->prsts = read32(PMC_BASE_ADDRESS + PRSTS);
+	ps->gen_pmcon1 = read32(PMC_BASE_ADDRESS + GEN_PMCON1);
+	ps->gen_pmcon2 = read32(PMC_BASE_ADDRESS + GEN_PMCON2);
+
+	ps->prev_sleep_state = chipset_prev_sleep_state(ps);
+
+	return ps;
+}
+
+/* Return 0, 3, or 5 to indicate the previous sleep state. */
+int chipset_prev_sleep_state(struct chipset_power_state *ps)
+{
+	/* Default to S0. */
+	int prev_sleep_state = SLEEP_STATE_S0;
+
+	if (ps->pm1_sts & WAK_STS) {
+		switch ((ps->pm1_cnt & SLP_TYP) >> SLP_TYP_SHIFT) {
+		case SLP_TYP_S5:
+			prev_sleep_state = SLEEP_STATE_S5;
+			break;
+		}
+
+		/* Clear SLP_TYP. */
+		outl(ps->pm1_cnt & ~(SLP_TYP), ACPI_BASE_ADDRESS + PM1_CNT);
+	}
+
+	if (ps->gen_pmcon1 & (PWR_FLR | SUS_PWR_FLR))
+		prev_sleep_state = SLEEP_STATE_S5;
+
+	return prev_sleep_state;
+}
+
 void ramstage_cache_invalid(struct ramstage_cache *cache)
 {
 }
@@ -91,6 +157,12 @@ void ramstage_cache_invalid(struct ramstage_cache *cache)
 asmlinkage void *romstage_main(unsigned int bist,
 			      uint32_t tsc_low, uint32_t tsc_high)
 {
+	struct romstage_params rp = {
+		.bist = bist,
+		.power_state = NULL,
+		.pei_data = NULL,
+	};
+
 	post_code(0x30);
 
 	/* Save timestamp information */
@@ -118,10 +190,61 @@ asmlinkage void *romstage_main(unsigned int bist,
 	printk(BIOS_DEBUG, "FSP TempRamInit successful\n");
 	print_fsp_info(find_fsp());
 
+	/* Continue chipset initialization */
+	spi_init();
+	set_max_freq();
+	gfx_init();
+
+#if IS_ENABLED(CONFIG_EC_GOOGLE_CHROMEEC)
+	/* Ensure the EC is in the right mode for recovery */
+	google_chromeec_early_init();
+#endif
+
+	/* Get power management information. */
+	rp.power_state = fill_power_state();
+
+	/* Call into mainboard. */
+	mainboard_romstage_entry(&rp);
+
 	printk(BIOS_ERR, "Hanging in romstage_main!\n");
 	post_code(0x35);
 	while (1)
 		;
 
 	return NULL;
+}
+
+void romstage_common(struct romstage_params *rp)
+{
+	timestamp_add_now(TS_BEFORE_INITRAM);
+
+	/* Fill in any extra pei_data fields */
+	rp->pei_data->boot_mode = rp->power_state->prev_sleep_state;
+
+#if IS_ENABLED(CONFIG_ELOG_BOOT_COUNT)
+	boot_count_increment();
+#endif
+
+	/* Check recovery and MRC cache */
+	rp->pei_data->saved_data_size = 0;
+	rp->pei_data->saved_data = NULL;
+	if (!rp->pei_data->disable_saved_data) {
+		if (recovery_mode_enabled()) {
+			/* Recovery mode does not use MRC cache */
+			printk(BIOS_DEBUG,
+			       "Recovery mode: not using MRC cache.\n");
+		} else {
+			printk(BIOS_DEBUG, "No MRC cache found.\n");
+#if IS_ENABLED(CONFIG_EC_GOOGLE_CHROMEEC)
+			if (rp->pei_data->boot_mode == SLEEP_STATE_S0) {
+				/* Ensure EC is running RO firmware. */
+				google_chromeec_check_ec_image(EC_IMAGE_RO);
+			}
+#endif
+		}
+	}
+
+	/* Perform RAM initialization */
+	raminit(rp, rp->pei_data);
+	timestamp_add_now(TS_AFTER_INITRAM);
 }
