098e15481f527ea6ada53ea02a3511de679e0491
Lee Leahy <leroy.p.leahy@intel.com>
2015-01-19 20:20:14 -0800

    Braswell: Add P-State support
    Update the Strago board configuration to enable P-States.
    
    BRANCH=none
    BUG=None
    TEST=Build and run on Strago
    TEST=Build and run on Strago
    1. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241784/
    2. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241724/
    3. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241771/
    4. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241757/
    5. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241359/
    6. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241770/
    7. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241782/
    8. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/240477/
    9. Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241723/
    10 Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241780/
    11 Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241820/
    12 Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241811/
    13 Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241818/
    14 Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241900/
    15 Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241901/
    16 Cherry-pick patch https://chromium-
    review.googlesource.com/#/c/241910/
    17 emerge-strago coreboot coreboot-private-files-strago depthcharge
    libpayload chromeos-bootimage
    18 Flash image.bin
    19 Boot strago
    20 Log into the console
    21 run cbmem -y
    22 The test is successful if the boot times are displayed
    
    Change-Id: I5954cf0d082381e174df76b02423ed4dc727621c
    Signed-off-by: Lee Leahy <Leroy.P.Leahy@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/241907
    Reviewed-by: Erik C Bjorge <erik.c.bjorge@intel.com>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Commit-Queue: Leroy P Leahy <leroy.p.leahy@intel.com>
    Tested-by: Leroy P Leahy <leroy.p.leahy@intel.com>

--- src/soc/intel/braswell/acpi.c
+++ src/soc/intel/braswell/acpi.c
@@ -217,6 +217,146 @@ void acpi_fill_in_fadt(acpi_fadt_t *fadt)
 	fadt->x_gpe1_blk.addrh = 0x0;
 }
 
+static int calculate_power(int tdp, int p1_ratio, int ratio)
+{
+	u32 m;
+	u32 power;
+
+	/*
+	 * M = ((1.1 - ((p1_ratio - ratio) * 0.00625)) / 1.1) ^ 2
+	 *
+	 * Power = (ratio / p1_ratio) * m * tdp
+	 */
+
+	m = (110000 - ((p1_ratio - ratio) * 625)) / 11;
+	m = (m * m) / 1000;
+
+	power = ((ratio * 100000 / p1_ratio) / 100);
+	power *= (m / 100) * (tdp / 1000);
+	power /= 1000;
+
+	return (int)power;
+}
+
+static int generate_p_state_entries(int core, int cores_per_package)
+{
+	int len, len_pss;
+	int ratio_min, ratio_max, ratio_turbo, ratio_step, ratio_range_2;
+	int coord_type, power_max, power_unit, num_entries;
+	int ratio, power, clock, clock_max;
+	int vid, vid_turbo, vid_min, vid_max, vid_range_2;
+	u32 control_status;
+	const struct pattrs *pattrs = pattrs_get();
+	msr_t msr;
+
+	/* Inputs from CPU attributes */
+	ratio_max = pattrs->iacore_ratios[IACORE_MAX];
+	ratio_min = pattrs->iacore_ratios[IACORE_LFM];
+	vid_max = pattrs->iacore_vids[IACORE_MAX];
+	vid_min = pattrs->iacore_vids[IACORE_LFM];
+
+	/* Set P-states coordination type based on MSR disable bit */
+	coord_type = (pattrs->num_cpus > 2) ? SW_ALL : HW_ALL;
+
+	/* Max Non-Turbo Frequency */
+	clock_max = (ratio_max * pattrs->bclk_khz) / 1000;
+
+	/* Calculate CPU TDP in mW */
+	msr = rdmsr(MSR_PKG_POWER_SKU_UNIT);
+	power_unit = 1 << (msr.lo & 0xf);
+	msr = rdmsr(MSR_PKG_POWER_LIMIT);
+	power_max = ((msr.lo & 0x7fff) / power_unit) * 1000;
+
+	/* Write _PCT indicating use of FFixedHW */
+	len = acpigen_write_empty_PCT();
+
+	/* Write _PPC with NVS specified limit on supported P-state */
+	len += acpigen_write_PPC_NVS();
+
+	/* Write PSD indicating configured coordination type */
+	len += acpigen_write_PSD_package(core, 1, coord_type);
+
+	/* Add P-state entries in _PSS table */
+	len += acpigen_write_name("_PSS");
+
+	/* Determine ratio points */
+	ratio_step = 1;
+	num_entries = (ratio_max - ratio_min) / ratio_step;
+	while (num_entries > 15) { /* ACPI max is 15 ratios */
+		ratio_step <<= 1;
+		num_entries >>= 1;
+	}
+
+	/* P[T] is Turbo state if enabled */
+	if (get_turbo_state() == TURBO_ENABLED) {
+		/* _PSS package count including Turbo */
+		len_pss = acpigen_write_package(num_entries + 2);
+
+		ratio_turbo = pattrs->iacore_ratios[IACORE_TURBO];
+		vid_turbo = pattrs->iacore_vids[IACORE_TURBO];
+		control_status = (ratio_turbo << 8) | vid_turbo;
+
+		/* Add entry for Turbo ratio */
+		len_pss += acpigen_write_PSS_package(
+			clock_max + 1,		/* MHz */
+			power_max,		/* mW */
+			10,			/* lat1 */
+			10,			/* lat2 */
+			control_status,		/* control */
+			control_status);	/* status */
+	} else {
+		/* _PSS package count without Turbo */
+		len_pss = acpigen_write_package(num_entries + 1);
+		ratio_turbo = ratio_max;
+		vid_turbo = vid_max;
+	}
+
+	/* First regular entry is max non-turbo ratio */
+	control_status = (ratio_max << 8) | vid_max;
+	len_pss += acpigen_write_PSS_package(
+		clock_max,		/* MHz */
+		power_max,		/* mW */
+		10,			/* lat1 */
+		10,			/* lat2 */
+		control_status,		/* control */
+		control_status);	/* status */
+
+	/* Set up ratio and vid ranges for VID calculation */
+	ratio_range_2 = (ratio_turbo - ratio_min) * 2;
+	vid_range_2 = (vid_turbo - vid_min) * 2;
+
+	/* Generate the remaining entries */
+	for (ratio = ratio_min + ((num_entries - 1) * ratio_step);
+	     ratio >= ratio_min; ratio -= ratio_step) {
+
+		/* Calculate VID for this ratio */
+		vid = ((ratio - ratio_min) * vid_range_2) /
+			ratio_range_2 + vid_min;
+		/* Round up if remainder */
+		if (((ratio - ratio_min) * vid_range_2) % ratio_range_2)
+			vid++;
+
+		/* Calculate power at this ratio */
+		power = calculate_power(power_max, ratio_max, ratio);
+		clock = (ratio * pattrs->bclk_khz) / 1000;
+		control_status = (ratio << 8) | (vid & 0xff);
+
+		len_pss += acpigen_write_PSS_package(
+			clock,			/* MHz */
+			power,			/* mW */
+			10,			/* lat1 */
+			10,			/* lat2 */
+			control_status,		/* control */
+			control_status);	/* status */
+	}
+
+	/* Fix package length */
+	len_pss--;
+	acpigen_patch_len(len_pss);
+
+	return len + len_pss;
+}
+
 void generate_cpu_entries(void)
 {
 	int len_pr, core;
@@ -232,6 +372,10 @@ void generate_cpu_entries(void)
 		/* Generate processor \_PR.CPUx */
 		len_pr = acpigen_write_processor(
 			core, pcontrol_blk, plen);
+
+		/* Generate  P-state tables */
+		len_pr += generate_p_state_entries(
+			core, pattrs->num_cpus);
 
 		len_pr--;
 		acpigen_patch_len(len_pr);
