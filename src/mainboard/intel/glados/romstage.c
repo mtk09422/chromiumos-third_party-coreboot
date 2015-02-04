a6ae82055b64f717737710ccf7a838895ee79876
Lee Leahy <leroy.p.leahy@intel.com>
2015-02-03 20:47:54 -0800

    Glados: MemoryInit
    Update files to enable successful execution of MemoryInit.
    Update the device tree.
    
    Serial output expected only after MemoryInit!
    Serial Jumpers: RX: J3B1 2-3, TX: J3B2 2-3
    
    BRANCH=none
    BUG=None
    TEST=Build and run on Glados
    1. Cherry-pick skylake TempRamInit patch
    2. Cherry-pick config glados MemoryInit patch
    3. Cherry-pick skylake MemoryInit patch
    4. emerge-glados  coreboot
    5. Flash coreboot.rom
    6. Test successful if post code is set to 0x35
    Test currently fails due to missing FSP_RESERVED_MEMORY_RESOURCE_HOB
    
    Change-Id: If63413d8dc92de38a868ccf82d796082b2da3f88
    Signed-off-by: Lee Leahy <Leroy.P.Leahy@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/247639
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Tested-by: Leroy P Leahy <leroy.p.leahy@intel.com>
    Commit-Queue: Leroy P Leahy <leroy.p.leahy@intel.com>

--- src/mainboard/intel/glados/romstage.c
+++ src/mainboard/intel/glados/romstage.c
@@ -18,9 +18,3 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
-#include <soc/romstage.h>
-
-void mainboard_romstage_entry(struct romstage_params *rp)
-{
-}
