a88a175f6d6db81d3154fb5dd31a44363ab94653
Andrew Bresticker <abrestic@chromium.org>
2015-02-05 13:48:35 -0800

    libpayload: mips: Use KSEG1 to access DMA-coherent memory
    Use bus_to_virt() to convert the physical address of the DMA
    coherent region to an address in KSEG1 which is suitable for
    device memory accesses.
    
    BUG=chrome-os-partner:36258
    BRANCH=none
    TEST=Build and boot on Pistachio.
    
    Change-Id: I9ad6435495df2c71d8f81a782f1c3dfcfd4aeb28
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/246696
    Reviewed-by: Ionela Voinescu <ionela.voinescu@imgtec.com>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
    Tested-by: Vadim Bendebury <vbendeb@chromium.org>

--- payloads/libpayload/arch/mips/coreboot.c
+++ payloads/libpayload/arch/mips/coreboot.c
@@ -27,7 +27,7 @@ void *cb_header_ptr;
 static void cb_parse_dma(void *ptr)
 {
 	struct cb_range *dma = (struct cb_range *)ptr;
-	init_dma_memory(phys_to_virt(dma->range_start), dma->range_size);
+	init_dma_memory(bus_to_virt(dma->range_start), dma->range_size);
 }
 
 /* Architecture specific */
