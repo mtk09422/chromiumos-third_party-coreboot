16d7cd273ae7dcd0bf8642457b278ab17ee52410
Yen Lin <yelin@nvidia.com>
2015-03-11 18:25:54 -0700

    foster: use MAX77620 pmic
    Change to use MAX77620 pmic, so cpu vdd can be turned on.
    
    BRANCH=None
    BUG=chrome-os-partner:37546
    TEST=build chromeos-bootimage by running
         emerge-foster chromeos-bootimage
    
    Change-Id: I5a36d54ef38e0cc2a268af2fed6af4ea99998a03
    Signed-off-by: Yen Lin <yelin@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/260081
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Jimmy Zhang <jimmzhang@nvidia.com>

--- src/mainboard/google/foster/pmic.c
+++ src/mainboard/google/foster/pmic.c
@@ -29,39 +29,22 @@
 #include "reset.h"
 
 enum {
-	AS3722_I2C_ADDR = 0x40
+	MAX77620_I2C_ADDR = 0x3c
 };
 
-struct as3722_init_reg {
+struct max77620_init_reg {
 	u8 reg;
 	u8 val;
 	u8 delay;
 };
 
-static struct as3722_init_reg init_list[] = {
-	{AS3722_SDO0, 0x3C, 1},
-	{AS3722_SDO1, 0x32, 0},
-	{AS3722_LDO3, 0x59, 0},
-	{AS3722_SDO2, 0x3C, 0},
-	{AS3722_SDO3, 0x00, 0},
-	{AS3722_SDO4, 0x00, 0},
-	{AS3722_SDO5, 0x50, 0},
-	{AS3722_SDO6, 0x28, 1},
-	{AS3722_LDO0, 0x8A, 0},
-	{AS3722_LDO1, 0x00, 0},
-	{AS3722_LDO2, 0x10, 0},
-	{AS3722_LDO4, 0x00, 0},
-	{AS3722_LDO5, 0x00, 0},
-	{AS3722_LDO6, 0x00, 0},
-	{AS3722_LDO7, 0x00, 0},
-	{AS3722_LDO9, 0x00, 0},
-	{AS3722_LDO10, 0x00, 0},
-	{AS3722_LDO11, 0x00, 1},
+static struct max77620_init_reg init_list[] = {
+	/* TODO */
 };
 
 static void pmic_write_reg(unsigned bus, uint8_t reg, uint8_t val, int delay)
 {
-	if (i2c_writeb(bus, AS3722_I2C_ADDR, reg, val)) {
+	if (i2c_writeb(bus, MAX77620_I2C_ADDR, reg, val)) {
 		printk(BIOS_ERR, "%s: reg = 0x%02X, value = 0x%02X failed!\n",
 			__func__, reg, val);
 		/* Reset the SoC on any PMIC write error */
@@ -76,37 +59,35 @@ static void pmic_slam_defaults(unsigned bus)
 {
 	int i;
 	for (i = 0; i < ARRAY_SIZE(init_list); i++) {
-		struct as3722_init_reg *reg = &init_list[i];
+		struct max77620_init_reg *reg = &init_list[i];
 		pmic_write_reg(bus, reg->reg, reg->val, reg->delay);
 	}
 }
 
 void pmic_init(unsigned bus)
 {
-	/*
-	 * Don't need to set up VDD_CORE - already done - by OTP
-	 * Don't write SDCONTROL - it's already 0x7F, i.e. all SDs enabled.
-	 * Don't write LDCONTROL - it's already 0xFF, i.e. all LDOs enabled.
-	 */
-
 	/* Restore PMIC POR defaults, in case kernel changed 'em */
 	pmic_slam_defaults(bus);
 
-	/* SDO0: Set VDD_CPU to 1.2V. */
-	pmic_write_reg(bus, 0x00, 0x50, 1);
+	/* Setup/Enable GPIO5 - VDD_CPU_REG_EN */
+	pmic_write_reg(bus, MAX77620_GPIO5_REG, 0x09, 1);
 
-	/* SDO6: Set VDD_GPU to 1.0V. */
-	pmic_write_reg(bus, 0x06, 0x28, 1);
+	/* Setup/Enable GPIO1 - VDD_HDMI_5V0_BST_EN -- ??? */
+	pmic_write_reg(bus, MAX77620_GPIO1_REG, 0x09, 1);
 
-	/* LDO2: Set +1.2V_GEN_AVDD to 1.2V */
-	pmic_write_reg(bus, 0x12, 0x10, 1);
+	/* GPIO 0,1,5,6,7 = GPIO, 2,3,4 = alt mode */
+	pmic_write_reg(bus, MAX77620_AME_GPIO, 0x1c, 1);
 
-	/*
-	 * Panel power GPIO O4. Set mode for GPIO4 (0x0c to 7), then set
-	 * the value (register 0x20 bit 4)
-	 */
-	pmic_write_reg(bus, 0x0c, 0x07, 0);
-	pmic_write_reg(bus, 0x20, 0x10, 1);
+	/* Disable SD1 Remote Sense, Set SD1 for LPDDR4 to 1.125v? */
+	pmic_write_reg(bus, MAX77620_CNFG2SD_REG, 0x04, 1);
+
+	pmic_write_reg(bus, MAX77620_SD1_REG, 0x2a, 1);
+
+	/* CNFG1_L2 = 0xF2 for 3.3v, enabled */
+	pmic_write_reg(bus, MAX77620_CNFG1_L2_REG, 0xf2, 1);
+
+	/* CNFG1_L1 = 0xCA for 1.05v, enabled */
+	pmic_write_reg(bus, MAX77620_CNFG1_L1_REG, 0xca, 1);
 
 	printk(BIOS_DEBUG, "PMIC init done\n");
 }
