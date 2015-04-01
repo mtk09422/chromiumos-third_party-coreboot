a7c9b0d7201b09a06ea32f0db84187d15f767c80
Lee Leahy <leroy.p.leahy@intel.com>
2015-03-31 17:30:01 -0700

    device: Add class and subclass name support
    Add support to display class and subclass names for PCI devices.
    
    BRANCH=none
    BUG=None
    TEST=Build and run on strago/cyan.
    
    Change-Id: Ibf2ee89dd84040ca6ab0e52857a69f7ed0c28f37
    Signed-off-by: Lee Leahy <Leroy.P.Leahy@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/263342
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Leroy P Leahy <leroy.p.leahy@intel.com>
    Tested-by: Leroy P Leahy <leroy.p.leahy@intel.com>

--- src/include/device/pci.h
+++ src/include/device/pci.h
@@ -81,6 +81,8 @@ void pci_dev_init(struct device *dev);
 
 void pci_assign_irqs(unsigned bus, unsigned slot,
 		     const unsigned char pIntAtoD[4]);
+const char *get_pci_class_name(device_t dev);
+const char *get_pci_subclass_name(device_t dev);
 
 #define PCI_IO_BRIDGE_ALIGN 4096
 #define PCI_MEM_BRIDGE_ALIGN (1024*1024)
