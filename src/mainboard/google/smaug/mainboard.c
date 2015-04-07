46a8e1f8718ddc69277807c91e99f2416a2beccf
Tom Warren <twarren@nvidia.com>
2015-04-07 16:33:36 -0700

    Smaug: audio: enable I2C6 and I2S1 for RT5677 codec, HP amp, etc.
    RT5677 audio codec is on I2C6, dev 0x2D. Uses GPIO_X1_AUD
    and DMIC1_DAT for CODEC_RST/AUDIO_ENABLE, I2S1(DAP1) for
    audio data from AHUB.
    
    Sound driver in depthcharge will make use of these devices.
    
    BUG=chrome-os-partner:38343
    BRANCH=None
    TEST=Smaug builds OK.
    
    Change-Id: Ice3db694f76240d54fc46456932654793db8e766
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/264511
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

--- src/mainboard/google/smaug/mainboard.c
+++ src/mainboard/google/smaug/mainboard.c
@@ -2,6 +2,7 @@
  * This file is part of the coreboot project.
  *
  * Copyright 2015 Google Inc.
+ * Copyright (c) 2015, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,33 +19,72 @@
  */
 
 #include <arch/mmu.h>
-#include <boardid.h>
 #include <boot/coreboot_tables.h>
-#include <cbmem.h>
 #include <device/device.h>
 #include <soc/addressmap.h>
 #include <soc/clk_rst.h>
 #include <soc/clock.h>
 #include <soc/funitcfg.h>
+#include <soc/nvidia/tegra/i2c.h>
 #include <soc/padconfig.h>
-
 #include "gpio.h"
-#include "pmic.h"
 
 static const struct pad_config padcfgs[] = {
 	PAD_CFG_GPIO_INPUT(USB_VBUS_EN1, PINMUX_PULL_NONE | PINMUX_PARKED |
 			   PINMUX_INPUT_ENABLE | PINMUX_LPDR | PINMUX_IO_HV),
 };
 
+static const struct pad_config audio_codec_pads[] = {
+	/* GPIO_X1_AUD(BB3) is CODEC_RST_L and DMIC1_DAT(E1) is AUDIO_ENABLE */
+	PAD_CFG_GPIO_OUT1(GPIO_X1_AUD, PINMUX_PULL_DOWN),
+	PAD_CFG_GPIO_OUT1(DMIC1_DAT, PINMUX_PULL_DOWN),
+};
+
+static const struct pad_config i2s1_pad[] = {
+	/* I2S1 */
+	PAD_CFG_SFIO(DAP1_SCLK, PINMUX_INPUT_ENABLE, I2S1),
+	PAD_CFG_SFIO(DAP1_FS, PINMUX_INPUT_ENABLE, I2S1),
+	PAD_CFG_SFIO(DAP1_DOUT, PINMUX_INPUT_ENABLE, I2S1),
+	PAD_CFG_SFIO(DAP1_DIN, PINMUX_INPUT_ENABLE | PINMUX_TRISTATE, I2S1),
+	/* codec MCLK via AUD SFIO */
+	PAD_CFG_SFIO(AUD_MCLK, PINMUX_PULL_NONE, AUD),
+};
+
+static const struct funit_cfg audio_funit[] = {
+	/* We need 1.5MHz for I2S1. So we use CLK_M */
+	FUNIT_CFG(I2S1, CLK_M, 1500, i2s1_pad, ARRAY_SIZE(i2s1_pad)),
+};
+
 static const struct funit_cfg funits[] = {
 	FUNIT_CFG_USB(USBD),
 	FUNIT_CFG(SDMMC4, PLLP, 48000, NULL, 0),
+	/* I2C6 for audio, temp sensor, etc. Enable codec via GPIOs/muxes */
+	FUNIT_CFG(I2C6, PLLP, 400, audio_codec_pads, ARRAY_SIZE(audio_codec_pads)),
 };
 
+/* Audio init: clocks and enables/resets */
+static void setup_audio(void)
+{
+	/* Audio codec (RT5677) uses OSC freq (via AUD_MCLK), s/b 38.4MHz */
+	soc_configure_funits(audio_funit, ARRAY_SIZE(audio_funit));
+
+	/*
+	 * As per NVIDIA hardware team, we need to take ALL audio devices
+	 * connected to AHUB (AUDIO, APB2APE, I2S, SPDIF, etc.) out of reset
+	 * and clock-enabled, otherwise reading AHUB devices (in our case,
+	 * I2S/APBIF/AUDIO<XBAR>) will hang.
+	 */
+	clock_enable_audio();
+}
 static void mainboard_init(device_t dev)
 {
 	soc_configure_pads(padcfgs, ARRAY_SIZE(padcfgs));
 	soc_configure_funits(funits, ARRAY_SIZE(funits));
+
+	/* I2C6 bus (audio, etc.) */
+	soc_configure_i2c6pad();
+	i2c_init(I2C6_BUS);
+	setup_audio();
 }
 
 static void mainboard_enable(device_t dev)
