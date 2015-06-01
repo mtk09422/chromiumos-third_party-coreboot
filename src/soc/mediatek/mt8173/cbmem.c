4b0b0a90b90527cd0b60a7dfb766b30a691f9bd7
CC Ma <cc.ma@mediatek.com>
2015-06-01 15:09:28 +0800

    mt8173: revise cbmem_top
    -Apply carveout method
    -Support memory range querying to above/below 4GiB
    
    BRANCH=none
    BUG=none
    TEST=build and verified pass on oak board
    
    Change-Id: I49f89f43d76d04f82c71200896eabdbe54eadab5
    Signed-off-by: CC Ma <cc.ma@mediatek.com>

--- src/soc/mediatek/mt8173/cbmem.c
+++ src/soc/mediatek/mt8173/cbmem.c
@@ -21,10 +21,22 @@
 #include <stddef.h>
 #include <symbols.h>
 #include <console/console.h>
+#include <soc/addressmap.h>
 
 void *cbmem_top(void)
 {
-	return (void *)((uintptr_t)_dram +
-			((uint64_t)CONFIG_DRAM_SIZE_MB << 20) -
-			((uint64_t)CONFIG_TRUSTZONE_CARVEOUT_SIZE_MB << 20));
+	static uintptr_t addr;
+
+	if (addr == 0) {
+		uintptr_t begin_mib;
+		uintptr_t end_mib;
+
+		memory_in_range_below_4gb(&begin_mib, &end_mib);
+		/* Make sure we consume everything up to 4GIB. */
+		if (end_mib == 4096)
+			addr = ~(uint32_t)0;
+		else
+			addr = end_mib << 20;
+	}
+	return (void *)addr;
 }
