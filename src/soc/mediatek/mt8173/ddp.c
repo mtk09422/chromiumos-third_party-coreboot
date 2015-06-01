7c91d6f7323f9b47f575d371972fa5648238d953
chiawen.lee <chiawen.lee@mediatek.com>
2015-06-01 15:09:48 +0800

    mt8173: Add display driver
    BRANCH=none
    BUG=none
    TEST=build and verified pass on oak board
    
    Change-Id: I4efe439d52b5a5516145960bcffb340152bfba53
    Signed-off-by: Chiawen Lee <chiawen.lee@mediatek.com>

---
+++ src/soc/mediatek/mt8173/ddp.c
@@ -0,0 +1,533 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2015 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <arch/io.h>
+#include <console/console.h>
+#include <delay.h>
+#include <edid.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <soc/addressmap.h>
+
+#include <soc/ddp.h>
+
+
+#define ddp_write32(data, addr)	\
+	write32((void *)((uintptr_t)(addr)), data)
+#define ddp_read32(addr) \
+	read32((void *)((uintptr_t)(addr)))
+
+#ifdef DEBUGDPF
+#define MTKFBTAG                "[MTKFB] "
+#define MTKFBERRTAG             "[MTKFB ERR] "
+#define MTKFBDPF(fmt, arg...)   printk(BIOS_INFO, MTKFBTAG fmt, ##arg)
+#define MTKFBERR(fmt, arg...)   printk(BIOS_INFO, MTKFBERRTAG fmt, ##arg)
+#else
+#define MTKFBERRTAG             "[MTKFB ERR] "
+#define MTKFBDPF(fmt, arg...)
+#define MTKFBERR(fmt, arg...)   printk(BIOS_INFO, MTKFBERRTAG fmt, ##arg)
+#endif
+
+
+static struct MTK_DISP_REGS _mtk_disp_regs = {
+	.regs       = DISPSYS_CONFIG_BASE,
+	.ovl_regs   = DISPSYS_OVL0_BASE,
+	.rdma_regs  = DISPSYS_RDMA0_BASE,
+	.color_regs = DISPSYS_COLOR0_BASE,
+	.aal_regs   = DISPSYS_AAL_BASE,
+	.ufoe_regs  = DISPSYS_UFOE_BASE,
+	.mutex_regs = DISPSYS_MUTEX_BASE,
+	.dsi_reg    = DISPSYS_DSI0_BASE,
+	.od_regs    = DISPSYS_OD_BASE,
+	.dsi_ana_reg = DSI_ANA_BASE,
+};
+
+/* These are locked by dev->vbl_lock */
+void mtk_enable_vblank(u32 disp_base)
+{
+	ddp_write32(0x1, disp_base + DISP_OD_INTEN);
+}
+
+void mtk_disable_vblank(u32 disp_base)
+{
+	ddp_write32(0x0, disp_base + DISP_OD_INTEN);
+}
+
+void mtk_clear_vblank(u32 disp_base)
+{
+	ddp_write32(0x0, disp_base + DISP_OD_INTS);
+}
+
+u32 mtk_get_vblank(u32 disp_base)
+{
+	return ddp_read32(disp_base + DISP_OD_INTS);
+}
+
+static void disp_config_main_path_connection(u32 disp_base)
+{
+	/* OVL0 output to COLOR0 */
+	ddp_write32(0x1, disp_base + DISP_REG_CONFIG_DISP_OVL0_MOUT_EN);
+
+	/* OD output to RDMA0 */
+	ddp_write32(0x1, disp_base + DISP_REG_CONFIG_DISP_OD_MOUT_EN);
+
+	/* UFOE output to DSI0 */
+	ddp_write32(0x1, disp_base + DISP_REG_CONFIG_DISP_UFOE_MOUT_EN);
+
+	/* COLOR0 input from OVL0 */
+	ddp_write32(0x1, disp_base + DISP_REG_CONFIG_DISP_COLOR0_SEL_IN);
+}
+
+static void disp_config_main_path_mutex(u32 mutex_base)
+{
+	unsigned int ID = 0, val;
+
+	/* Module: OVL0=11, RDMA0=13, COLOR0=18, AAL=20, UFOE=22, OD=25 */
+	val = (1 << 11 | 1 << 13 | 1 << 18 | 1 << 20 | 1 << 22 | 1 << 25);
+	ddp_write32(val, mutex_base + DISP_REG_CONFIG_MUTEX_MOD(ID));
+
+	/* Clock source from DSI0 */
+	ddp_write32(1, mutex_base + DISP_REG_CONFIG_MUTEX_SOF(ID));
+	ddp_write32(1, mutex_base + DISP_REG_CONFIG_MUTEX_EN(ID));
+}
+
+/* work on OVL0 only now */
+static void ovl_start(u32 ovl_base)
+{
+	ddp_write32(0x01, ovl_base + DISP_REG_OVL_EN);
+	ddp_write32(0x0f, ovl_base + DISP_REG_OVL_INTEN);
+}
+
+static void ovl_stop(u32 ovl_base)
+{
+	ddp_write32(0x00, ovl_base + DISP_REG_OVL_INTEN);
+	ddp_write32(0x00, ovl_base + DISP_REG_OVL_EN);
+	ddp_write32(0x00, ovl_base + DISP_REG_OVL_INTSTA);
+}
+
+static void ovl_set_roi(u32 ovl_base, unsigned int w, unsigned int h,
+	unsigned int color)
+{
+	ddp_write32(h << 16 | w, ovl_base + DISP_REG_OVL_ROI_SIZE);
+	ddp_write32(color, ovl_base + DISP_REG_OVL_ROI_BGCLR);
+}
+
+static void ovl_layer_switch(u32 ovl_base, unsigned layer, int en)
+{
+	u32 reg;
+
+	reg = ddp_read32(ovl_base + DISP_REG_OVL_SRC_CON);
+
+	if (en)
+		reg |= (1U<<layer);
+	else
+		reg &= ~(1U<<layer);
+
+	ddp_write32(reg, ovl_base + DISP_REG_OVL_SRC_CON);
+	ddp_write32(0x1, ovl_base + DISP_REG_OVL_RST);
+	ddp_write32(0x0, ovl_base + DISP_REG_OVL_RST);
+}
+
+static void rdma_start(u32 rdma_base, unsigned idx)
+{
+	unsigned int reg;
+	unsigned int offset = idx * RDMA_N_OFST;
+
+	if (idx >= 2)
+		return;
+
+	ddp_write32(0x4, rdma_base + DISP_REG_RDMA_INT_ENABLE + offset);
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+	reg |= 1;
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+}
+
+static void rdma_stop(u32 rdma_base, unsigned idx)
+{
+	unsigned int reg;
+	unsigned int offset = idx * RDMA_N_OFST;
+
+	if (idx >= 2)
+		return;
+
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+	reg &= ~(1U);
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+	ddp_write32(0, rdma_base + DISP_REG_RDMA_INT_ENABLE + offset);
+	ddp_write32(0, rdma_base + DISP_REG_RDMA_INT_STATUS + offset);
+}
+
+static void rdma_reset(u32 rdma_base, unsigned idx)
+{
+	unsigned int reg;
+	unsigned int delay_cnt;
+	unsigned int offset = idx * RDMA_N_OFST;
+
+	if (idx >= 2)
+		return;
+
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+	reg |= 0x10;
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+
+	delay_cnt = 0;
+	while ((ddp_read32(rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset)
+		& 0x700) == 0x100) {
+
+		delay_cnt++;
+		if (delay_cnt > 10000) {
+			MTKFBERR("rdma_reset timeout\n");
+			break;
+		}
+	}
+
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+	reg &= ~(0x10U);
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+
+	delay_cnt = 0;
+	while ((ddp_read32(rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset)
+		& 0x700) != 0x100) {
+
+		delay_cnt++;
+		if (delay_cnt > 10000) {
+			MTKFBERR("rdma_reset timeout\n");
+			break;
+		}
+	}
+}
+
+static void rdma_config_direct_link(u32 rdma_base, unsigned idx,
+	unsigned width, unsigned height)
+{
+	unsigned int reg;
+	enum RDMA_MODE mode = RDMA_MODE_DIRECT_LINK;
+	enum RDMA_OUTPUT_FORMAT output_format = RDMA_OUTPUT_FORMAT_ARGB;
+	unsigned int offset = idx * RDMA_N_OFST;
+
+	if (idx >= 2)
+		return;
+
+	/* Config mode */
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+	if (mode == RDMA_MODE_DIRECT_LINK)
+		reg &= ~(0x2U);
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_GLOBAL_CON + offset);
+
+	/* Config output format */
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_SIZE_CON_0 + offset);
+	if (output_format == RDMA_OUTPUT_FORMAT_ARGB)
+		reg &= ~(0x20000000U);
+	else
+		reg |= 0x20000000U;
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_SIZE_CON_0 + offset);
+
+	/* Config width */
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_SIZE_CON_0 + offset);
+	reg = (reg & ~(0xFFFU)) | (width & 0xFFFU);
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_SIZE_CON_0 + offset);
+
+	/* Config height */
+	reg = ddp_read32(rdma_base + DISP_REG_RDMA_SIZE_CON_1 + offset);
+	reg = (reg & ~(0xFFFFFU)) | (height & 0xFFFFFU);
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_SIZE_CON_1 + offset);
+
+	/* Config FIFO control */
+	reg = 0x80F00008;
+	ddp_write32(reg, rdma_base + DISP_REG_RDMA_FIFO_CON + offset);
+}
+
+static void od_start(u32 od_base, unsigned int W, unsigned int H)
+{
+	ddp_write32(W << 16 | H, od_base + DISP_OD_SIZE);
+	ddp_write32(1, od_base + DISP_OD_CFG); /* RELAY mode */
+
+	ddp_write32(1, od_base + DISP_OD_EN);
+}
+
+static void ufoe_start(u32 ufoe_base)
+{
+	ddp_write32(0x4, ufoe_base + DISPSYS_UFOE_BASE);
+}
+
+static void color_start(u32 color_base)
+{
+	ddp_write32(0x2080, color_base + DISP_COLOR_CFG_MAIN);
+	ddp_write32(0x1, color_base + DISP_COLOR_START);
+}
+
+static void ovl_layer_config(unsigned int fmt, unsigned int addr,
+			   unsigned int width, unsigned int height)
+{
+	u32 drm_disp_base = _mtk_disp_regs.ovl_regs;
+	unsigned int reg;
+	unsigned int source = 0;	/* from memory */
+	unsigned int src_x = 0;	/* ROI x offset */
+	unsigned int src_y = 0;	/* ROI y offset */
+	unsigned int src_pitch;
+	unsigned int dst_x = 0;	/* ROI x offset */
+	unsigned int dst_y = 0;	/* ROI y offset */
+	unsigned int dst_w = width;	/* ROT width */
+	unsigned int dst_h = height;	/* ROI height */
+	int color_key_enable = 1;
+	unsigned int key = 0xFF000000;	/* color key */
+	int aen = 0;			/* alpha enable */
+	unsigned char alpha = 0x0;
+	unsigned int rgb_swap, bpp;
+
+	if (fmt == OVL_INFMT_BGR888 || fmt == OVL_INFMT_BGR565 ||
+		fmt == OVL_INFMT_ABGR8888 || fmt == OVL_INFMT_BGRA8888) {
+		fmt -= OVL_COLOR_BASE;
+		rgb_swap = 1;
+	} else {
+		rgb_swap = 0;
+	}
+
+	switch (fmt) {
+	case OVL_INFMT_ARGB8888:
+	case OVL_INFMT_RGBA8888:
+		bpp = 4;
+		break;
+	case OVL_INFMT_RGB888:
+		bpp = 3;
+		break;
+	case OVL_INFMT_RGB565:
+	case OVL_INFMT_YUYV:
+	case OVL_INFMT_UYVY:
+		bpp = 2;
+		break;
+	default:
+		bpp = 1;	/* invalid input format */
+	}
+
+	src_pitch = width * bpp;
+	ovl_layer_switch(drm_disp_base, 0, 1);
+
+	ddp_write32(0x1, drm_disp_base + DISP_REG_OVL_RST);
+	ddp_write32(0x0, drm_disp_base + DISP_REG_OVL_RST);
+
+	ddp_write32(0x3, drm_disp_base + DISP_REG_OVL_SRC_CON);
+
+	ddp_write32(0x00000001, drm_disp_base + DISP_REG_OVL_RDMA0_CTRL);
+	ddp_write32(0x40402020,
+		    drm_disp_base + DISP_REG_OVL_RDMA0_MEM_GMC_SETTING);
+
+	reg = color_key_enable << 30 | source << 28 | rgb_swap << 25 |
+	      fmt << 12 | aen << 8 | alpha;
+	ddp_write32(reg, drm_disp_base + DISP_REG_OVL_L0_CON);
+	ddp_write32(key, drm_disp_base + DISP_REG_OVL_L0_SRCKEY);
+	ddp_write32(dst_h << 16 | dst_w,
+		    drm_disp_base + DISP_REG_OVL_L0_SRC_SIZE);
+	ddp_write32(dst_y << 16 | dst_x,
+		    drm_disp_base + DISP_REG_OVL_L0_OFFSET);
+	reg = addr + src_x * bpp + src_y * src_pitch;
+	ddp_write32(reg, drm_disp_base + DISP_REG_OVL_L0_ADDR);
+	ddp_write32(src_pitch & 0xFFFF,
+		    drm_disp_base + DISP_REG_OVL_L0_PITCH);
+}
+
+void ovl_dump_register(u32 disp_base)
+{
+	int i;
+
+	MTKFBDPF("OVL Register:\n");
+	for (i = 0; i < 0x300; i += 16)
+		MTKFBDPF("%#08X: %08X %08X %08X %08X\n", i,
+			ddp_read32(disp_base + DISPSYS_OVL0_BASE + i),
+			ddp_read32(disp_base + DISPSYS_OVL0_BASE + i + 4),
+			ddp_read32(disp_base + DISPSYS_OVL0_BASE + i + 8),
+			ddp_read32(disp_base + DISPSYS_OVL0_BASE + i + 12));
+}
+
+void rdma_dump_register(u32 disp_base)
+{
+	int i;
+
+	MTKFBDPF("RDMA Register:\n");
+	for (i = 0; i < 0x100; i += 16)
+		MTKFBDPF("%#08X: %08X %08X %08X %08X\n", i,
+			ddp_read32(disp_base + DISPSYS_RDMA0_BASE + i),
+			ddp_read32(disp_base + DISPSYS_RDMA0_BASE + i + 4),
+			ddp_read32(disp_base + DISPSYS_RDMA0_BASE + i + 8),
+			ddp_read32(disp_base + DISPSYS_RDMA0_BASE + i + 12));
+}
+
+void disp_config_dump_register(u32 disp_base)
+{
+	int i;
+
+	MTKFBDPF("Disp Config Register:\n");
+	for (i = 0; i < 0x180; i += 16)
+		MTKFBDPF("%#08X: %08X %08X %08X %08X\n", i,
+			ddp_read32(disp_base + DISPSYS_CONFIG_BASE + i),
+			ddp_read32(disp_base + DISPSYS_CONFIG_BASE + i + 4),
+			ddp_read32(disp_base + DISPSYS_CONFIG_BASE + i + 8),
+			ddp_read32(disp_base + DISPSYS_CONFIG_BASE + i + 12));
+}
+
+void main_disp_path_power_on(void)
+{
+	ovl_start(_mtk_disp_regs.ovl_regs);
+	rdma_start(_mtk_disp_regs.rdma_regs, 0);
+}
+
+void main_disp_path_power_off(void)
+{
+	ovl_stop(_mtk_disp_regs.ovl_regs);
+	rdma_stop(_mtk_disp_regs.rdma_regs, 0);
+	rdma_reset(_mtk_disp_regs.rdma_regs, 0);
+
+	disp_config_dump_register(_mtk_disp_regs.regs);
+}
+
+static void main_disp_path_setup(const struct edid *edid)
+{
+
+	unsigned int width, height;
+
+	width = edid->ha;
+	height = edid->va;
+
+	MTKFBDPF("main_disp_path_setup %d %d\n", width, height);
+	/* Setup OVL */
+	ovl_set_roi(_mtk_disp_regs.ovl_regs, width, height, 0x00000000);
+	ovl_layer_switch(_mtk_disp_regs.ovl_regs, 0, 1);
+
+	/* Setup RDMA0 */
+	rdma_config_direct_link(_mtk_disp_regs.rdma_regs, 0, width, height);
+
+	/* Setup OD */
+	od_start(_mtk_disp_regs.od_regs, width, height);
+
+	/* Setup UFOE */
+	ufoe_start(_mtk_disp_regs.ufoe_regs);
+
+	/* Setup Color */
+	color_start(_mtk_disp_regs.color_regs);
+
+	/* Setup main path connection */
+	disp_config_main_path_connection(_mtk_disp_regs.regs);
+
+	/* Setup main path mutex */
+	disp_config_main_path_mutex(_mtk_disp_regs.mutex_regs);
+}
+
+void main_disp_path_clear(void)
+{
+	int i;
+
+	MTKFBDPF("main_disp_path_clear..all\n");
+	for (i = 0; i < 0x100; i += 4)
+		ddp_write32(0, _mtk_disp_regs.regs + i);
+
+	for (i = 0; i < 0x1000; i += 4) /* OVL */
+		ddp_write32(0, _mtk_disp_regs.ovl_regs + i);
+
+	for (i = 0; i < 0x1000; i += 4) /* RDMA */
+		ddp_write32(0, _mtk_disp_regs.rdma_regs + i);
+
+	for (i = 0x400; i < 0x1000; i += 4) /* COLOR */
+		ddp_write32(0, _mtk_disp_regs.color_regs + i);
+
+	for (i = 0; i < 0x1000; i += 4) /* AAL */
+		ddp_write32(0, _mtk_disp_regs.aal_regs + i);
+
+	for (i = 0; i < 0x1000; i += 4) /* UFOE */
+		ddp_write32(0, _mtk_disp_regs.ufoe_regs + i);
+
+	for (i = 0; i < 0x1000; i += 4) /* DSI */
+		ddp_write32(0, _mtk_disp_regs.dsi_reg + i);
+
+	for (i = 0; i < 0x1000; i += 4) /* MUTEX */
+		ddp_write32(0, _mtk_disp_regs.mutex_regs + i);
+
+	for (i = 0; i < 0x1000; i += 4) /* OD */
+		ddp_write32(0, _mtk_disp_regs.od_regs + i);
+}
+
+static void disp_clock_on(void)
+{
+	u32 addr;
+	u32 cg_con;
+
+	MTKFBDPF("disp_clock_on : force enable all\n");
+	addr = _mtk_disp_regs.regs + DISP_REG_CONFIG_MMSYS_CG_CON0;
+	cg_con = ddp_read32(addr);
+	cg_con = cg_con & (~CG_CON0_SMI_COMMON) & (~CG_CON0_SMI_LARB0) &
+		  (~CG_CON0_MUTEX_32K) & (~CG_CON0_DISP_OVL0) &
+		  (~CG_CON0_DISP_RDMA0) & (~CG_CON0_DISP_COLOR0) &
+		  (~CG_CON0_DISP_OD);
+
+	ddp_write32(cg_con, addr);
+
+	addr = _mtk_disp_regs.regs + DISP_REG_CONFIG_MMSYS_CG_CON1;
+	cg_con = ddp_read32(addr);
+	cg_con = cg_con & (~CG_CON1_DSI0_ENGINE) & (~CG_CON1_DSI0_DIGITAL);
+
+	ddp_write32(cg_con, addr);
+}
+
+void disp_clock_clear(u32 drm_disp_base)
+{
+	unsigned int reg;
+
+	/* ovl_disp_ck */
+	reg = ddp_read32(drm_disp_base + DISP_REG_CONFIG_MMSYS_CG_CON0);
+
+	ddp_write32(reg, drm_disp_base + DISP_REG_CONFIG_MMSYS_CG_CON0);
+}
+
+void mtk_ddp_init(void)
+{
+	MTKFBDPF("mtk_ddp_init\n");
+
+	mtk_enable_vblank(_mtk_disp_regs.od_regs);
+	disp_clock_on();
+}
+
+void mtk_ddp_mode_set(u32 fbbase, const struct edid *edid)
+{
+	unsigned int fmt;
+
+	MTKFBDPF("mtk_ddp_mode_set fb %dX%dX%d @%08x\n",
+		 edid->ha, edid->va, edid->framebuffer_bits_per_pixel, fbbase);
+
+	main_disp_path_setup(edid);
+
+	if (fbbase != 0)
+		ovl_start(_mtk_disp_regs.ovl_regs);
+
+	rdma_start(_mtk_disp_regs.rdma_regs, 0);
+
+	switch (edid->framebuffer_bits_per_pixel) {
+	case 16:
+		fmt = OVL_INFMT_RGB565;
+		break;
+	case 24:
+		fmt = OVL_INFMT_RGB888;
+		break;
+	case 32:
+	default:
+		fmt = OVL_INFMT_RGBA8888;
+		break;
+	}
+
+	ovl_layer_config(fmt, fbbase, edid->ha, edid->va);
+}
---
+++ src/soc/mediatek/mt8173/include/soc/dsi.h
@@ -0,0 +1,413 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2015 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _DSI_REG_H_
+#define _DSI_REG_H_
+
+#define BIT(nr)                 (1UL << (nr))
+
+/* video mode */
+enum {
+	MIPI_DSI_MODE_VIDEO = BIT(0),
+	/* video burst mode */
+	MIPI_DSI_MODE_VIDEO_BURST = BIT(1),
+	/* video pulse mode */
+	MIPI_DSI_MODE_VIDEO_SYNC_PULSE = BIT(2),
+	/* enable auto vertical count mode */
+	MIPI_DSI_MODE_VIDEO_AUTO_VERT = BIT(3),
+	/* enable hsync-end packets in vsync-pulse and v-porch area */
+	MIPI_DSI_MODE_VIDEO_HSE = BIT(4),
+	/* disable hfront-porch area */
+	MIPI_DSI_MODE_VIDEO_HFP = BIT(5),
+	/* disable hback-porch area */
+	MIPI_DSI_MODE_VIDEO_HBP = BIT(6),
+	/* disable hsync-active area */
+	MIPI_DSI_MODE_VIDEO_HSA = BIT(7),
+	/* flush display FIFO on vsync pulse */
+	MIPI_DSI_MODE_VSYNC_FLUSH = BIT(8),
+	/* disable EoT packets in HS mode */
+	MIPI_DSI_MODE_EOT_PACKET = BIT(9),
+	/* device supports non-continuous clock behavior (DSI spec 5.6.1) */
+	MIPI_DSI_CLOCK_NON_CONTINUOUS = BIT(10),
+	/* transmit data in low power */
+	MIPI_DSI_MODE_LPM = BIT(11)
+};
+
+enum mipi_dsi_pixel_format {
+	MIPI_DSI_FMT_RGB888,
+	MIPI_DSI_FMT_RGB666,
+	MIPI_DSI_FMT_RGB666_PACKED,
+	MIPI_DSI_FMT_RGB565,
+};
+
+
+/* video/viedomode.h */
+struct videomode {
+	unsigned long pixelclock;
+	u32 hactive;
+	u32 hfront_porch;
+	u32 hback_porch;
+	u32 hsync_len;
+	u32 vactive;
+	u32 vfront_porch;
+	u32 vback_porch;
+	u32 vsync_len;
+
+	u32 flags; /* display flags */
+};
+
+enum mtk_dsi_format {
+	MTK_DSI_FORMAT_16P,
+	MTK_DSI_FORMAT_18NP,
+	MTK_DSI_FORMAT_18P,
+	MTK_DSI_FORMAT_24P,
+};
+
+enum {
+	DSI_START	= 0x00,
+	DSI_INTEN	= 0x08,
+	DSI_INTSTA	= 0x0C,
+	DSI_CON_CTRL	= 0x10,
+	DSI_MODE_CTRL   = 0x14,
+	DSI_TXRX_CTRL	= 0x18,
+	DSI_PSCTRL	= 0x1C,
+	DSI_VSA_NL   = 0x20,
+	DSI_VBP_NL   = 0x24,
+	DSI_VFP_NL   = 0x28,
+	DSI_VACT_NL  = 0x2C,
+	DSI_HSA_WC   = 0x50,
+	DSI_HBP_WC   = 0x54,
+	DSI_HFP_WC   = 0x58,
+	DSI_BLLP_WC  = 0x5C,
+	DSI_PHY_LCCON = 0x104,
+	DSI_PHY_LD0CON = 0x108
+};
+
+/* DSI_INTSTA */
+enum {
+	RD_RDY		= (1),
+	CMD_DONE	= (1<<1),
+	TE_RDY		= (1<<2),
+	VM_DONE		= (1<<3),
+	EXT_TE		= (1<<4),
+	VM_CMD_DONE	= (1<<5),
+	BUSY		= (1<<31)
+};
+
+/* DSI_CON_CTRL */
+enum {
+	DSI_RESET	= (1),
+};
+
+/* DSI_MODE_CTRL */
+enum {
+	MODE = 2,
+	CMD_MODE = 0,
+	SYNC_PULSE_MODE = 1,
+	SYNC_EVENT_MODE = 2,
+	BURST_MODE = 3,
+	FRM_MODE = (1<<16),
+	MIX_MODE = (1<<17),
+};
+
+/* DSI_TXRX_CTRL */
+enum {
+	VC_NUM	= (2<<0),
+	LANE_NUM = (0xF<<2),
+	DIS_EOT	 = (1<<6),
+	NULL_EN	 = (1<<7),
+	TE_FREERUN = (1<<8),
+	EXT_TE_EN  = (1<<9),
+	EXT_TE_EDGE = (1<<10),
+	MAX_RTN_SIZE = (0xF<<12),
+	HSTX_CKLP_EN = (1<<16)
+};
+
+/* DSI_PSCTRL */
+enum {
+	DSI_PS_WC = 0x3FFF,
+	DSI_PS_SEL = (2<<16),
+	PACKED_PS_16BIT_RGB565	= (0<<16),
+	LOOSELY_PS_18BIT_RGB666	= (1<<16),
+	PACKED_PS_18BIT_RGB666	= (2<<16),
+	PACKED_PS_24BIT_RGB888	= (3<<16)
+};
+
+/* DSI_BLLP_WC */
+enum {
+	DSI_HSTX_CKL_WC	= 0x64
+};
+
+/* DSI_PHY_LCCON */
+enum {
+	LC_HS_TX_EN = (1),
+	LC_ULPM_EN  = (1<<1),
+	LC_WAKEUP_EN = (1<<2)
+};
+
+/*DSI_PHY_LD0CON */
+enum {
+	LD0_HS_TX_EN = (1),
+	LD0_ULPM_EN = (1<<1),
+	LD0_WAKEUP_EN = (1<<2)
+};
+
+/* MIPITX_REG */
+enum {
+	MIPITX_DSI0_CON		= 0x00,
+	MIPITX_DSI0_CLOCK_LANE	= 0x04,
+	MIPITX_DSI0_DATA_LANE0	= 0x08,
+	MIPITX_DSI0_DATA_LANE1	= 0x0C,
+	MIPITX_DSI0_DATA_LANE2	= 0x10,
+	MIPITX_DSI0_DATA_LANE3	= 0x14,
+	MIPITX_DSI_TOP_CON	= 0x40,
+	MIPITX_DSI_BG_CON	= 0x44,
+	MIPITX_DSI_PLL_CON0	= 0x50,
+	MIPITX_DSI_PLL_PWR	= 0x68
+};
+
+/* MIPITX_DSI0_CON */
+enum {
+	RG_DSI0_LDOCORE_EN	= (1),
+	RG_DSI0_CKG_LDOOUT_EN	= (1<<1),
+	RG_DSI0_BCLK_SEL	= (3<<2),
+	RG_DSI0_LD_IDX_SEL	= (7<<4),
+	RG_DSI0_PHYCLK_SEL	= (2<<8),
+	RG_DSI0_DSICLK_FREQ_SEL	= (1<<10),
+	RG_DSI0_LPTX_CLMP_EN	= (1<<11)
+};
+
+/* MIPITX_DSI0_CLOCK_LANE */
+enum {
+	RG_DSI0_LNTC_LDOOUT_EN	 = (1),
+	RG_DSI0_LNTC_CKLANE_EN	 = (1<<1),
+	RG_DSI0_LNTC_LPTX_IPLUS1 = (1<<2),
+	RG_DSI0_LNTC_LPTX_IPLUS2 = (1<<3),
+	RG_DSI0_LNTC_LPTX_IMINUS = (1<<4),
+	RG_DSI0_LNTC_LPCD_IPLUS	 = (1<<5),
+	RG_DSI0_LNTC_LPCD_IMLUS	 = (1<<6),
+	RG_DSI0_LNTC_RT_CODE	 = (0xF<<8)
+};
+
+/* MIPITX_DSI0_DATA_LANE0 */
+enum {
+	RG_DSI0_LNT0_LDOOUT_EN	 = (1),
+	RG_DSI0_LNT0_CKLANE_EN	 = (1<<1),
+	RG_DSI0_LNT0_LPTX_IPLUS1 = (1<<2),
+	RG_DSI0_LNT0_LPTX_IPLUS2 = (1<<3),
+	RG_DSI0_LNT0_LPTX_IMINUS = (1<<4),
+	RG_DSI0_LNT0_LPCD_IPLUS	 = (1<<5),
+	RG_DSI0_LNT0_LPCD_IMINUS = (1<<6),
+	RG_DSI0_LNT0_RT_CODE	 = (0xF<<8)
+};
+
+
+/* MIPITX_DSI0_DATA_LANE1 */
+enum {
+	RG_DSI0_LNT1_LDOOUT_EN	 = (1),
+	RG_DSI0_LNT1_CKLANE_EN	 = (1<<1),
+	RG_DSI0_LNT1_LPTX_IPLUS1 = (1<<2),
+	RG_DSI0_LNT1_LPTX_IPLUS2 = (1<<3),
+	RG_DSI0_LNT1_LPTX_IMINUS = (1<<4),
+	RG_DSI0_LNT1_LPCD_IPLUS	 = (1<<5),
+	RG_DSI0_LNT1_LPCD_IMINUS = (1<<6),
+	RG_DSI0_LNT1_RT_CODE	 = (0xF<<8)
+};
+
+/* MIPITX_DSI0_DATA_LANE2 */
+enum {
+	RG_DSI0_LNT2_LDOOUT_EN	 = (1),
+	RG_DSI0_LNT2_CKLANE_EN	 = (1<<1),
+	RG_DSI0_LNT2_LPTX_IPLUS1 = (1<<2),
+	RG_DSI0_LNT2_LPTX_IPLUS2 = (1<<3),
+	RG_DSI0_LNT2_LPTX_IMINUS = (1<<4),
+	RG_DSI0_LNT2_LPCD_IPLUS	 = (1<<5),
+	RG_DSI0_LNT2_LPCD_IMINUS = (1<<6),
+	RG_DSI0_LNT2_RT_CODE	 = (0xF<<8)
+};
+
+/* MIPITX_DSI0_DATA_LANE3 */
+enum {
+	RG_DSI0_LNT3_LDOOUT_EN	 = (1),
+	RG_DSI0_LNT3_CKLANE_EN	 = (1<<1),
+	RG_DSI0_LNT3_LPTX_IPLUS1 = (1<<2),
+	RG_DSI0_LNT3_LPTX_IPLUS2 = (1<<3),
+	RG_DSI0_LNT3_LPTX_IMINUS = (1<<4),
+	RG_DSI0_LNT3_LPCD_IPLUS	 = (1<<5),
+	RG_DSI0_LNT3_LPCD_IMINUS = (1<<6),
+	RG_DSI0_LNT3_RT_CODE	 = (0xF<<8)
+};
+
+/* MIPITX_DSI_TOP_CON */
+enum {
+	RG_DSI_LNT_INTR_EN	= (1),
+	RG_DSI_LNT_HS_BIAS_EN	= (1<<1),
+	RG_DSI_LNT_IMP_CAL_EN	= (1<<2),
+	RG_DSI_LNT_TESTMODE_EN	= (1<<3),
+	RG_DSI_LNT_IMP_CAL_CODE	= (0xF<<4),
+	RG_DSI_LNT_AIO_SEL	= (7<<8),
+	RG_DSI_PAD_TIE_LOW_EN	= (1<<11),
+	RG_DSI_DEBUG_INPUT_EN	= (1<<12),
+	RG_DSI_PRESERVE		= (7<<13)
+};
+
+/* MIPITX_DSI_BG_CON */
+enum {
+	RG_DSI_BG_CORE_EN	= 1,
+	RG_DSI_BG_CKEN		= (1<<1),
+	RG_DSI_BG_DIV		= (0x3<<2),
+	RG_DSI_BG_FAST_CHARGE	= (1<<4),
+	RG_DSI_V12_SEL		= (7<<5),
+	RG_DSI_V10_SEL		= (7<<8),
+	RG_DSI_V072_SEL		= (7<<11),
+	RG_DSI_V04_SEL		= (7<<14),
+	RG_DSI_V032_SEL		= (7<<17),
+	RG_DSI_V02_SEL		= (7<<20),
+	rsv_23			= (1<<23),
+	RG_DSI_BG_R1_TRIM	= (0xF<<24),
+	RG_DSI_BG_R2_TRIM	= (0xF<<28)
+};
+
+/* MIPITX_DSI_PLL_CON0 */
+enum {
+	RG_DSI0_MPPLL_PLL_EN	= (1<<0),
+	RG_DSI0_MPPLL_PREDIV	= (3<<1),
+	RG_DSI0_MPPLL_TXDIV0	= (3<<3),
+	RG_DSI0_MPPLL_TXDIV1	= (3<<5),
+	RG_DSI0_MPPLL_POSDIV	= (7<<7),
+	RG_DSI0_MPPLL_MONVC_EN	= (1<<10),
+	RG_DSI0_MPPLL_MONREF_EN	= (1<<11),
+	RG_DSI0_MPPLL_VOD_EN	= (1<<12)
+};
+
+/* MIPITX_DSI_PLL_PWR */
+enum {
+	RG_DSI_MPPLL_SDM_PWR_ON	 = (1<<0),
+	RG_DSI_MPPLL_SDM_ISO_EN	 = (1<<1),
+	RG_DSI_MPPLL_SDM_PWR_ACK = (1<<8)
+};
+
+enum {
+	DSI_PHY_TIMECON0 = 0x0110,
+	DSI_PHY_TIMECON1 = 0x0114,
+	DSI_PHY_TIMECON2 = 0x0118,
+	DSI_PHY_TIMECON3 = 0x011C
+};
+
+enum {
+	LPX	= (0xFF<<0),
+	HS_PRPR	= (0xFF<<8),
+	HS_ZERO	= (0xFF<<16),
+	HS_TRAIL = (0xFF<<24),
+
+	TA_GO	 = (0xFF<<0),
+	TA_SURE	 = (0xFF<<8),
+	TA_GET	 = (0xFF<<16),
+	DA_HS_EXIT = (0xFF<<24),
+
+	CONT_DET = (0xFF<<0),
+	CLK_ZERO = (0xFF<<16),
+	CLK_TRAIL = (0xFF<<24),
+
+	CLK_HS_PRPR = (0xFF<<0),
+	CLK_HS_POST = (0xFF<<8),
+	CLK_HS_EXIT = (0xFF<<16),
+};
+
+/* MIPITX_DSI_PLL_CON1 */
+enum {
+	RG_DSI0_MPPLL_SDM_FRA_EN	= (1),
+	RG_DSI0_MPPLL_SDM_SSC_PH_INIT	= (1<<1),
+	RG_DSI0_MPPLL_SDM_SSC_EN	= (1<<2),
+	RG_DSI0_MPPLL_SDM_SSC_PRD	= (0xFFFF<<16)
+};
+
+enum {
+	MIPITX_DSI_PLL_CON1	= 0x54,
+	MIPITX_DSI_PLL_CON2	= 0x58,
+	MIPITX_DSI_PLL_CON3	= 0x5C,
+	MIPITX_DSI_PLL_TOP	= 0x60,
+	MIPITX_DSI_RGS		= 0x64,
+	MIPITX_DSI_GPIO_EN	= 0x68,
+	MIPITX_DSI_GPIO_OUT	= 0x6c,
+	MIPITX_DSI_SW_CTRL	= 0x80,
+	MIPITX_DSI_SW_CTRL_CON0	= 0x84,
+	MIPITX_DSI_SW_CTRL_CON1	= 0x88,
+	MIPITX_DSI0_MPPLL_SDM_CON1 = 0x90,
+	MIPITX_DSI0_MPPLL_SDM_CON2 = 0x94
+};
+
+/* MIPITX_DSI_PLL_TOP */
+enum {
+	RG_MPPLL_TST_EN	= (1),
+	RG_MPPLL_TSTCK_EN = (1<<1),
+	RG_MPPLL_TSTSEL	= (3<<2),
+	RG_MPPLL_PRESERVE = (0xFF<<16),
+};
+
+/* MIPITX_DSI_SW_CTRL */
+enum {
+	SW_CTRL_EN = (1<<0),
+};
+
+/* MIPITX_DSI_SW_CTRL_CON0 */
+enum {
+	SW_LNTC_LPTX_PRE_OE	= (1<<0),
+	SW_LNTC_LPTX_OE		= (1<<1),
+	SW_LNTC_LPTX_P		= (1<<2),
+	SW_LNTC_LPTX_N		= (1<<3),
+	SW_LNTC_HSTX_PRE_OE	= (1<<4),
+	SW_LNTC_HSTX_OE		= (1<<5),
+	SW_LNTC_HSTX_ZEROCLK	= (1<<6),
+	SW_LNT0_LPTX_PRE_OE	= (1<<7),
+	SW_LNT0_LPTX_OE		= (1<<8),
+	SW_LNT0_LPTX_P		= (1<<9),
+	SW_LNT0_LPTX_N		= (1<<10),
+	SW_LNT0_HSTX_PRE_OE	= (1<<11),
+	SW_LNT0_HSTX_OE		= (1<<12),
+	SW_LNT0_LPRX_EN		= (1<<13),
+	SW_LNT1_LPTX_PRE_OE	= (1<<14),
+	SW_LNT1_LPTX_OE		= (1<<15),
+	SW_LNT1_LPTX_P		= (1<<16),
+	SW_LNT1_LPTX_N		= (1<<17),
+	SW_LNT1_HSTX_PRE_OE	= (1<<18),
+	SW_LNT1_HSTX_OE		= (1<<19),
+	SW_LNT2_LPTX_PRE_OE	= (1<<20),
+	SW_LNT2_LPTX_OE		= (1<<21),
+	SW_LNT2_LPTX_P		= (1<<22),
+	SW_LNT2_LPTX_N		= (1<<23),
+	SW_LNT2_HSTX_PRE_OE	= (1<<24),
+	SW_LNT2_HSTX_OE		= (1<<25)
+};
+
+/* MIPITX_DSI0_MPPLL_SDM_CON1 */
+enum {
+	RG_DSI0_MPPLL_ISO_EN =	(1),
+	RG_BY_ISO_DLY	= (1<<1),
+	RG_DSI0_MPPLL_PWR_ON = (1<<2),
+	RG_BY_PWR_DLY	     = (1<<3),
+	RSV_ATPG_CON         = (1<<4),
+	DSI0_MPPLL_PWR_ACK   = (1<<24)
+};
+
+int mtk_output_dsi_enable(void);
+int mtk_output_dsi_disable(void);
+int mtk_dsi_int(struct videomode *mode);
+
+#endif
