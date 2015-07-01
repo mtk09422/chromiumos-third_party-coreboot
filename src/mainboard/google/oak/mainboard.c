9108d7d99fe0cfb5b1cbaf639feb6f47287253b0
Koro Chen <koro.chen@mediatek.com>
2015-07-01 19:47:22 +0800

    Oak: config regulators for external audio codec
    BRANCH=none
    BUG=none
    TEST=build and verified pass on oak board
    
    Change-Id: I848468cec04a36659fbb4b898dff9368305d72ac
    Signed-off-by: Koro Chen <koro.chen@mediatek.com>

--- src/mainboard/google/oak/mainboard.c
+++ src/mainboard/google/oak/mainboard.c
@@ -26,8 +26,41 @@
 #include <gpio.h>
 #include <soc/display.h>
 #include <soc/ddp.h>
+#include <soc/pmic.h>
 #include <soc/spm.h>
 #include <soc/usb.h>
+
+static void configure_regulator(void)
+{
+	switch (board_id()) {
+	case 0:
+		/* vgp1 set to 1.22V */
+		mt6391_write(PMIC_RG_DIGLDO_CON19, 0, PMIC_RG_VCAMD_VOSEL_MASK,
+			     PMIC_RG_VCAMD_VOSEL_SHIFT);
+		mt6391_write(PMIC_RG_DIGLDO_CON30, 0, PMIC_RG_VCAMD_ON_CTRL_MASK,
+			     PMIC_RG_VCAMD_ON_CTRL_SHIFT);
+		mt6391_write(PMIC_RG_DIGLDO_CON5, 1, PMIC_RG_VCAMD_SW_EN_MASK,
+			     PMIC_RG_VCAMD_SW_EN_SHIFT);
+		/* vgp4 set to 1.8V */
+		mt6391_write(PMIC_RG_DIGLDO_CON22, 3, PMIC_RG_VGP4_VOSEL_MASK,
+			     PMIC_RG_VGP4_VOSEL_SHIFT);
+		mt6391_write(PMIC_RG_DIGLDO_CON30, 0, PMIC_RG_VGP4_ON_CTRL_MASK,
+			     PMIC_RG_VGP4_ON_CTRL_SHIFT);
+		mt6391_write(PMIC_RG_DIGLDO_CON8, 1, PMIC_RG_VGP4_SW_EN_MASK,
+			     PMIC_RG_VGP4_SW_EN_SHIFT);
+		break;
+	default:
+		/* board from Rev1 */
+		/* vgp1 set to 1.8V */
+		mt6391_write(PMIC_RG_DIGLDO_CON19, 3, PMIC_RG_VCAMD_VOSEL_MASK,
+			     PMIC_RG_VCAMD_VOSEL_SHIFT);
+		mt6391_write(PMIC_RG_DIGLDO_CON30, 0, PMIC_RG_VCAMD_ON_CTRL_MASK,
+			     PMIC_RG_VCAMD_ON_CTRL_SHIFT);
+		mt6391_write(PMIC_RG_DIGLDO_CON5, 1, PMIC_RG_VCAMD_SW_EN_MASK,
+			     PMIC_RG_VCAMD_SW_EN_SHIFT);
+		break;
+	}
+}
 
 static void configure_hdmi(void)
 {
@@ -65,6 +98,7 @@ static void mainboard_init(device_t dev)
 	usb_port_switch();
 	configure_hdmi();
 	configure_vpu_sram();
+	configure_regulator();
 }
 
 static void mainboard_enable(device_t dev)
